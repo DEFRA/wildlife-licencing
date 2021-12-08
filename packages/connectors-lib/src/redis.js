17ffd340741df92c20ce7561f92ea15cd6910857
GrahamMWillis <94051723+GrahamMWillis@users.noreply.github.com>
2021-12-08 12:52:38 +0000

    Add redis database parameter to allow different connections to use different database numbers (#15)
    Co-authored-by: GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>

--- packages/connectors-lib/src/redis.js
+++ packages/connectors-lib/src/redis.js
@@ -6,12 +6,16 @@ let client
 export const REDIS = {
   getClient: () => client,
   initialiseConnection: async () => {
-    client = createClient({
+    client = createClient(Object.assign({
       socket: {
         host: Config.redis.host,
         port: Config.redis.port
       }
-    })
+    }, Config.redis.database
+      ? {
+          database: Config.redis.database
+        }
+      : {}))
     await client.connect()
     return Promise.resolve()
   }
