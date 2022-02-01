19dc85960f27c9d847be98dde153253968b9b405
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2022-02-01 10:33:41 +0000

    Add the putSite handler

--- packages/api/src/handlers/site/site.js
+++ packages/api/src/handlers/site/site.js
@@ -1,7 +1,9 @@
 import postSite from './post-site.js'
+import putSite from './put-site.js'
 import getSitesByUserId from './get-sites-by-user-id.js'
 
 export {
+  getSitesByUserId,
   postSite,
-  getSitesByUserId
+  putSite
 }
