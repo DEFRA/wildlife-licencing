8aaa3ade277885d2ad310c2701452fd0d493a66e
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2022-02-02 09:06:53 +0000

    Fight sonar cube

--- packages/api/src/handlers/utils.js
+++ packages/api/src/handlers/utils.js
@@ -1,26 +1,6 @@
 import { models } from '@defra/wls-database-model'
 import { cache } from '../services/cache.js'
 
-// export const utils = async (context, req, h, userId) => {
-//   const user = await models.users.findByPk(userId)
-//
-//   // Check the user exists
-//   if (!user) {
-//     return h.response().code(404)
-//   }
-//
-//   // Check cache
-//   const saved = await cache.restore(req.path)
-//
-//   if (saved) {
-//     return h.response(JSON.parse(saved))
-//       .type(APPLICATION_JSON)
-//       .code(200)
-//   }
-//
-//   return null
-// }
-
 /**
  * Convenience functions
  * @param context
