24f7578469c145d3f7e20eefaf3b8da7f91524e7
Brad <brad@128bitstudios.co.uk>
2022-02-21 23:56:47 +0000

    remove joi model for application

--- packages/web-service/src/pages/applications/route.js
+++ packages/web-service/src/pages/applications/route.js
@@ -1,25 +1,13 @@
 import pageRoute from '../../routes/page-route.js'
 import { APPLICATIONS } from '../../uris.js'
 import fetch from 'node-fetch'
-import { ApplicationsSchema } from '../../models/application.js'
 
 const userId = '579d4e05-e9d8-472f-a9a9-3fc52234d88b'
 
 async function fetchApplications(userId) {
   try {
-    // todo: need to create a service file for common fetch functions
-    // todo: replace localhost string with process env value
-    const fetchedApplications =
-      await fetch(`http://localhost:3000/user/${userId}/applications`)
-        .then((response) => response.json())
-
-    const validatedFetchedApplications = ApplicationsSchema.validate(fetchedApplications)
-
-    if (validatedFetchedApplications.error) {
-      throw new Error(validatedFetchedApplications.error)
-    }
-
-    return validatedFetchedApplications.value
+    return await fetch(`http://localhost:3000/user/${userId}/applications`)
+      .then((response) => response.json())
   } catch (exception) {
     console.error(exception)
   }
