50993dfd2c88e439d2b6d6934161a0ff2f5ecd66
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2022-03-08 15:31:43 +0000

    Add debug and remove secretes looks

--- packages/connectors-lib/src/redis.js
+++ packages/connectors-lib/src/redis.js
@@ -1,11 +1,15 @@
 import { createClient } from 'redis'
 import Config from './config.js'
+import db from 'debug'
+const debug = db('connectors-lib:db')
 
 let client
 
 export const REDIS = {
   getClient: () => client,
   initialiseConnection: async () => {
+    debug(`Redis host: ${Config.redis.host}`)
+    debug(`Redis port: ${Config.redis.port}`)
     const options = {
       socket: {
         host: Config.redis.host,
