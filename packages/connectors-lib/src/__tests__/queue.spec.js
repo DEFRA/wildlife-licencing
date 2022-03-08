cba90560e111910bf3996918dbbbb47dc158882c
GrahamMWillis <94051723+GrahamMWillis@users.noreply.github.com>
2022-03-08 15:37:39 +0000

    Add debug and remove secretes looks (#41)
    Co-authored-by: GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>

--- packages/connectors-lib/src/__tests__/queue.spec.js
+++ packages/connectors-lib/src/__tests__/queue.spec.js
@@ -1,24 +1,7 @@
 
 describe('The queue connector', () => {
   beforeEach(() => jest.resetModules())
-  it('fetches override paramaters', async () => {
-    jest.doMock('../config.js', () => ({
-      queue: {
-        host: 'queue-host',
-        port: 'queue-port',
-        database: 'queue-database'
-      }
-    }))
-    const { QUEUE } = await import('../queue.js')
-    const c = QUEUE.connection
-    expect(c).toEqual({
-      database: 'queue-database',
-      host: 'queue-host',
-      port: 'queue-port'
-    })
-  })
-
-  it('fallsback to redis paramaters', async () => {
+  it('uses the redis parameters', async () => {
     jest.doMock('../config.js', () => ({
       queue: {},
       redis: {
--- packages/connectors-lib/src/queue.js
+++ packages/connectors-lib/src/queue.js
@@ -2,8 +2,8 @@ import Config from './config.js'
 
 export const QUEUE = {
   connection: {
-    host: Config.queue.host || Config.redis.host,
-    port: Config.queue.port || Config.redis.port,
-    database: Config.queue.database || Config.redis.database
+    host: Config.redis.host,
+    port: Config.redis.port,
+    database: Config.redis.database
   }
 }
