786199573b38952e155690905aa298c427e754b5
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2022-03-09 08:57:19 +0000

    Prepare for running within CRON

--- packages/application-extract-processor/entrypoint.sh
+++ packages/application-extract-processor/entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 #####################################################################
-# Used in the local swarm to initiate the cron job
-# Cron needs to have the environment in its own shell
+# Used to initiate the cron job - Cron needs to have the environment
+# in its own shell.
+#
+# Run once first to initialize
 #####################################################################
 env >> /etc/environment
 
+echo Initializing...
+cd /app && node src/processor.js
+
 # execute CMD
+echo Running cron...
 echo "$@"
 exec "$@"
