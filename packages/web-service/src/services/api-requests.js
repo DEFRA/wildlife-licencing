f301a76175022276afc92515730f7ca9d2341887
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2022-03-28 15:20:34 +0100

    Add some debug to look at login problem in production

--- packages/web-service/src/services/api-requests.js
+++ packages/web-service/src/services/api-requests.js
@@ -1,9 +1,12 @@
 import { API } from '@defra/wls-connectors-lib'
+import db from 'debug'
+const debug = db('web-service:api-requests')
 
 export const APIRequests = {
   USER: {
     findByName: async username => {
       try {
+        debug(`Finding user by username: ${username}`)
         const users = await API.get('/users', `username=${username}`)
         return users.length === 1 ? users[0] : null
       } catch (error) {
@@ -13,6 +16,7 @@ export const APIRequests = {
     },
     create: async username => {
       try {
+        debug(`Creating new user: ${username}`)
         await API.post('/user', { username })
       } catch (error) {
         console.error(`Error creating user ${username}`, error)
@@ -23,6 +27,7 @@ export const APIRequests = {
   APPLICATION: {
     create: async (userId, type) => {
       try {
+        debug(`Creating new application of type: ${type} for userId: ${userId}`)
         const { ref: applicationReferenceNumber } = await API.get('/applications/get-reference', `applicationType=${type}`)
         return API.post(`/user/${userId}/application`, { applicationReferenceNumber, applicationType: type })
       } catch (error) {
@@ -32,6 +37,7 @@ export const APIRequests = {
     },
     findByUser: async userId => {
       try {
+        debug(`Finding applications for userId: ${userId}`)
         return API.get(`/user/${userId}/applications`)
       } catch (error) {
         console.error(`Error finding application with userId ${userId}`, error)
