286f0775ba7da14c710846b8604bbd5d92f44c99
Parr <kealan.parr@capgemini.com>
2022-06-27 16:59:57 +0100

    fix: .txt files now work across edge, firefox and chrome. Now ensuring other file formats also work

--- packages/web-service/src/pages/file-upload/file-upload.js
+++ packages/web-service/src/pages/file-upload/file-upload.js
@@ -1,12 +1,53 @@
 import handler from '../../handlers/page-handler.js'
 import { FILE_UPLOAD } from '../../uris.js'
+import fs from 'fs'
+import * as StreamPromises from 'stream/promises'
+
+async function * transformer (source) {
+  for await (const chunk of source) {
+    const buff = Buffer.from(chunk, 'utf8')
+
+    // This may look a peculiar thing to do to a file we are uploading, but on a form upload, browsers delimit the content with header + footer tags
+    // To ensure we match the file (byte for byte) with what the user has uploaded - we need to trim out the delimiters the browser is adding (from the Content-Type header)
+    //
+    // Each regex has been written to be defensively match, to ensure no genuine content will be removed by it
+    // Also operates on every chunk that gets sent to ensure if the headers are sent in 2 seperate chunks - it won't matter
+    // As we are running three independent, seperate regexes - it works if one chunk or seven are sent
+    //
+    // If you need to change this code, the headers that Firefox and Webkit based browsers (Chrome + Edge) delimit with are different, and need to be tested
+    // Here are what the 3 different browsers all look like: https://regexr.com/6oj1o
+    //
+    let result = buff.toString().replace(/------.*[\r\n]+Content-Disposition: form-data; name="file-upload"; filename=".*/m, '')
+    result = result.replace(/[\r\n]+Content-Type: .*[\r\n]+/m, '')
+    result = result.replace(/------.*[\r\n]+Content-Disposition: form-data; name="continue"[\r\n]+------.*\s+/m, '')
+    result = stripFinalNewline(result)
+
+    yield result
+  }
+}
+
+// Streams output a single newline at the end of the stream
+// As we need to match the file byte for byte - need to trim it
+const stripFinalNewline = input => {
+  const LF = typeof input === 'string' ? '\n' : '\n'.charCodeAt()
+  const CR = typeof input === 'string' ? '\r' : '\r'.charCodeAt()
+
+  if (input[input.length - 1] === LF) {
+    input = input.slice(0, -1)
+  }
+
+  if (input[input.length - 1] === CR) {
+    input = input.slice(0, -1)
+  }
+
+  return input
+}
 
 const setData = async request => {
   // Hapi adds filename meta-data...
-  // The content is delimited in by the boundary given in the content-type header
-
-  // Payload contains a readable stream....
-  request.payload.pipe(process.stdout)
+  const writeStream = fs.createWriteStream('out.txt', { flags: 'w' })
+  await StreamPromises.pipeline(request.payload, transformer, writeStream)
+  await request.payload.pipe(writeStream)
 }
 
 const fileUploadPageRoute = (view, path, checkData, getData, completion, setData) => [
@@ -33,7 +74,7 @@ const fileUploadPageRoute = (view, path, checkData, getData, completion, setData
         payload: true
       },
       payload: {
-        maxBytes: 1048576,
+        maxBytes: 30000000,
         output: 'stream',
         parse: false,
         multipart: {
