8895673b1b7fee9dd8a84d7e84b990483568085a
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2022-07-13 08:33:32 +0100

    Work-in-progress

--- packages/application-extract-processor/src/__tests__/write-habitat-site-object.spec.js
+++ packages/application-extract-processor/src/__tests__/write-habitat-site-object.spec.js
@@ -35,6 +35,81 @@ const keys = [
 describe('The application extract processor: write-habitat-site-object', () => {
   beforeEach(() => jest.resetModules())
 
-  it('makes no change if an habitat-site has no sites', async () => {
+  it('does nothing if no application key found', async () => {
+    const { writeHabitatSiteObject } = await import('../write-habitat-site-object.js')
+    const result = await writeHabitatSiteObject({ data: {}, keys: [] }, null)
+    expect(result).toEqual({ error: 0, insert: 0, pending: 0, update: 0 })
   })
+
+  it('does nothing if the application is not found in the database', async () => {
+    const { models } = await import('@defra/wls-database-model')
+    models.applications = {
+      findOne: jest.fn(() => null)
+    }
+    const { writeHabitatSiteObject } = await import('../write-habitat-site-object.js')
+    const result = await writeHabitatSiteObject({
+      data: {},
+      keys: [
+        {
+          apiTable: 'applications',
+          powerAppsKey: '694b683d-85bc-4685-b1c5-d9a4708fa642'
+        }
+      ]
+    }, null)
+    expect(result).toEqual({ error: 0, insert: 0, pending: 0, update: 0 })
+  })
+
+  it('creates a new habitat-site', async () => {
+    const { models } = await import('@defra/wls-database-model')
+    const mockCreate = jest.fn()
+    models.applications = {
+      findOne: jest.fn(() => ({ id: 'fedb14b6-53a8-ec11-9840-0022481aca85' }))
+    }
+    models.habitatSites = {
+      findOne: jest.fn(() => null),
+      create: mockCreate
+    }
+    const { writeHabitatSiteObject } = await import('../write-habitat-site-object.js')
+    const result = await writeHabitatSiteObject({ data, keys }, null)
+    expect(mockCreate).toHaveBeenCalledWith({
+      applicationId: 'fedb14b6-53a8-ec11-9840-0022481aca85',
+      habitatSite: {
+        endDate: '2022-02-03',
+        name: 'Desctructive',
+        startDate: '2022-01-29'
+      },
+      id: expect.any(String),
+      sddsHabitatSiteId: '843a2a44-3180-ec11-8d21-000d3a0ca1c0',
+      updateStatus: 'U'
+    })
+    expect(result).toEqual({ error: 0, insert: 1, pending: 0, update: 0 })
+  })
+
+  it.only('makes an update on a found, pending habitat-site with a timestamp older than the extract start time', async () => {
+    const { models } = await import('@defra/wls-database-model')
+    const mockUpdate = jest.fn()
+    models.applications = {
+      findOne: jest.fn(() => ({ id: 'fedb14b6-53a8-ec11-9840-0022481aca85' }))
+    }
+    models.habitatSites = {
+      findOne: jest.fn(() => ({
+        id: '9487013e-abf5-4f42-95fa-15ad404570a1',
+        updateStatus: 'P',
+        updatedAt: new Date(2020, 0, 1),
+        habitatSite: data.habitatSite
+      })),
+      update: mockUpdate
+    }
+    const { writeHabitatSiteObject } = await import('../write-habitat-site-object.js')
+    const result = await writeHabitatSiteObject({ data, keys }, new Date())
+    expect(mockUpdate).toHaveBeenCalledWith({
+      habitatSite: data.habitatSite,
+      updateStatus: 'U'
+    }, { returning: false, where: { id: '9487013e-abf5-4f42-95fa-15ad404570a1' } })
+    expect(result).toEqual({ error: 0, insert: 0, pending: 0, update: 1 })
+  })
+
+  it('makes an update on a found, unlocked habitat-site, if data has changed', async () => {})
+  it('ignores an update on a found, unlocked habitat-site, if data has not changed', async () => {})
+  it('does not make an update on a found, pending habitat-site with a timestamp newer than the extract', async () => {})
 })
