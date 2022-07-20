1d043821521d8b74751866d5b5a5def835784b64
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2022-07-20 14:57:58 +0100

    Whoops! missed a commit....

--- packages/powerapps-lib/src/schema/tables/sdds-licensable-actions.js
+++ packages/powerapps-lib/src/schema/tables/sdds-licensable-actions.js
@@ -2,6 +2,7 @@ import { Column, OperationType, Relationship, RelationshipType, Table } from '..
 
 export const SddsLicensableActions = new Table('sdds_licensableactions', [
   new Column('sdds_method', 'methodIds', m => m.join(','), m => m && m.split(',').map(m2 => parseInt(m2))),
+  new Column('sdds_species', 'name'), // Note: error in the data-verse
   new Column('sdds_osgridref', 'gridReference'),
   new Column('sdds_setttype', 'settType'),
   new Column('sdds_proposedstartdate', 'startDate'),
@@ -16,10 +17,10 @@ export const SddsLicensableActions = new Table('sdds_licensableactions', [
 
   new Relationship('sdds_licenseActivity_sdds_licenseactivity', 'sdds_licenseactivities',
     RelationshipType.MANY_TO_ONE, 'sdds_licenseactivityid',
-    'activityId', null, null, OperationType.INBOUND, true),
+    'activityId', null, null, OperationType.INBOUND_AND_OUTBOUND, true),
 
   new Relationship('sdds_licensableaction_sdds_specieid_sdds_', 'sdds_species',
     RelationshipType.MANY_TO_ONE, 'sdds_specieid',
-    'speciesId', null, null, OperationType.INBOUND, true)
+    'speciesId', null, null, OperationType.INBOUND_AND_OUTBOUND, true)
 
 ], 'habitatSite', 'habitatSites', 'sdds_licensableactionid')
