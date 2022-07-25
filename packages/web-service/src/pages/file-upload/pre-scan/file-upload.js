1bf93421837c75d7713442bdb0b44853e52aada3
sfaulkner89 <s.faulkner89@gmail.com>
2022-07-25 11:22:44 +0100

    pushing to Graham

---
+++ packages/web-service/src/pages/file-upload/pre-scan/file-upload.js
@@ -0,0 +1,141 @@
+import handler, { errorShim } from '../../../handlers/page-handler.js'
+import { CHECK_YOUR_ANSWERS, FILE_UPLOAD } from '../../../uris.js'
+import { scanFile } from '../../../services/virus-scan.js'
+import Joi from 'joi'
+import fs from 'fs'
+
+const setData = async (request) => {
+  const currentFilePlusDirectory = process.env.SCANNING_DIR + '/' + request.payload['scan-file'].path.split('/').pop()
+
+  // We need to take a filename like: hello.txt
+  // And rename it to something like: {unixTimestamp}.{originalFileName}
+  // So if the user uploads it to us, but doesn't submit it fully all the way to s3 - we can check the timestamp and delete it
+  // It also stops collisions if multiple users all upload files with the same name
+  const newFilename = (+new Date()) + '.' + request.payload['scan-file'].filename
+  const newFilenamePlusDirectory = process.env.SCANNING_DIR + '/' + newFilename
+
+  fs.renameSync(currentFilePlusDirectory, newFilenamePlusDirectory, (err) => {
+    if (err) { console.err(err) }
+  })
+
+  const virusPresent = false
+  if (virusPresent) {
+    await request.cache().setPageData({
+      payload: request.payload,
+      error: errorShim(new Joi.ValidationError('ValidationError', [{
+        message: 'Error: the file contains a virus',
+        path: ['scan-file'],
+        type: 'infected',
+        context: {
+          label: 'scan-file',
+          value: 'Error',
+          key: 'scan-file'
+        }
+      }]))
+    })
+  } else {
+    await request.cache().setPageData({
+      fileName: newFilename,
+      path: newFilenamePlusDirectory
+    })
+  }
+}
+
+const completion = async (request) => {
+  const journeyData = await request.cache().getPageData() || {}
+  const { applicationId } = await request.cache().getData()
+  if (journeyData.error) {
+    return FILE_UPLOAD.uri
+  } else {
+    await request.cache().setData(Object.assign(await request.cache().getPageData() || {}, {
+      filename: request.payload['scan-file'].filename,
+      applicationId: applicationId
+    }))
+    return CHECK_YOUR_ANSWERS.uri
+  }
+}
+
+export const validator = async payload => {
+  // The user hasn't attached a file in their request
+  if (payload['scan-file'].bytes === 0 && payload['scan-file'].filename === '') {
+    // Hapi generates a has for a filename, and still attempts to store what the user has sent (an empty file)
+    // In this instance, we need to wipe the temporary file and throw a joi error
+    fs.unlinkSync(payload['scan-file'].path, err => {
+      if (err) throw err
+    })
+
+    throw new Joi.ValidationError('ValidationError', [{
+      message: 'Error: no file has been uploaded',
+      path: ['scan-file'],
+      type: 'no-file-chosen',
+      context: {
+        label: 'scan-file',
+        value: 'Error',
+        key: 'scan-file'
+      }
+    }], null)
+  }
+
+  if (payload['scan-file'].bytes >= 30_000_000) {
+    fs.unlinkSync(payload['scan-file'].path, err => {
+      if (err) throw err
+    })
+
+    throw new Joi.ValidationError('ValidationError', [{
+      message: 'Error: the file was too large',
+      path: ['scan-file'],
+      type: 'file-too-big',
+      context: {
+        label: 'scan-file',
+        value: 'Error',
+        key: 'scan-file'
+      }
+    }], null)
+  }
+}
+
+const fileUploadPageRoute = (view, path, checkData, getData, validator, completion, setData) => [
+  {
+    method: 'GET',
+    path: path,
+    handler: handler(view, checkData, getData).get
+  },
+  {
+    method: 'POST',
+    path: path,
+    handler: async (request, h) => {
+      if (setData) {
+        await setData(request)
+      }
+      if (typeof completion === 'function') {
+        return h.redirect(await completion(request))
+      } else {
+        return h.redirect(completion)
+      }
+    },
+    options: {
+      validate: {
+        payload: validator,
+        failAction: handler().error
+      },
+      plugins: {
+        disinfect: false
+      },
+      payload: {
+        // maxBytes defaults to one megabyte (which we need to be bigger)
+        // But we also need to catch the error and raise a joi error (rather than let hapi catch it)
+        // So for files 30 - 40mb - we'll raise a joi error
+        // For files 40mb and above - we won't even accept the file, we'll just redirect the user to an error page inside `client-error.njk`
+        maxBytes: (process.env.MAX_FILE_UPLOAD * 1_333_334),
+        uploads: process.env.SCANNING_DIR,
+        multipart: {
+          output: 'file'
+        },
+        parse: true,
+        timeout: 10000
+      }
+    }
+  }
+]
+
+export const fileUpload = fileUploadPageRoute(FILE_UPLOAD.page, FILE_UPLOAD.uri, null, null, validator, completion, setData)
---
+++ packages/web-service/src/services/__tests__/s3-upload.spec.js
@@ -0,0 +1,30 @@
+
+describe('The virus scanning service', () => {
+  beforeEach(() => jest.resetModules())
+  describe('S3 Bucket File Upload', () => {
+    it('Throws an error if a new bucket cannot be created', async () => {
+      const mockHead = () => Promise.reject(new Error())
+      const mockCreate = () => Promise.reject(new Error())
+      const errSpy = jest.spyOn(console, 'error')
+      jest.doMock('@aws-sdk/client-s3', () => ({
+        S3: jest.fn().mockImplementation(() => ({
+            headBucket: jest.fn((bucketName, callback) => mockHead),
+            createBucket: jest.fn((bucketName, callback) => mockCreate)
+          }))
+        }))
+      const { S3FileUpload } = await import('../s3-upload.js')
+      await S3FileUpload('0b357917-9b23-4f16-b460-bcee0ff1103f', 'file.txt', '/scandir/file.txt')
+      await expect(errSpy).toHaveBeenCalledTimes(1)
+      await expect(mockHead).toHaveBeenCalledTimes(1)
+    })
+    // it('Creates bucket if application bucket does not already exist and uploads file to new bucke', async () => {
+    //   const mockCreate = { data: 'data goes here' }
+    //   const mockUpload = { location: }
+    //   jest.doMock('@aws-sdk/client-s3', () => jest.fn().mockImplementation(() => {
+    //     return ({ headBucket: () => mockCreate })
+    //   }))
+    //   const { S3FileUpload } = await import('../s3-upload.js')
+    //   await expect(() => S3FileUpload('0b357917-9b23-4f16-b460-bcee0ff1103f', 'file.txt', '/scandir/file.txt')).rejects.toThrow()
+    // })
+  })
+})
