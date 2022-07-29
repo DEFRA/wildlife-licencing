51ac381772edc4b5c4fd6d4ae913d774722883d6
Parr <kealan.parr@capgemini.com>
2022-07-29 16:28:06 +0100

    fix: habitat start page wire up

---
+++ packages/web-service/src/pages/habitat/habitat-start.js
@@ -0,0 +1,4 @@
+import pageRoute from '../../routes/page-route.js'
+import { habitatURIs } from '../../uris.js'
+
+export default pageRoute({ page: habitatURIs.START.page, uri: habitatURIs.START.uri })
