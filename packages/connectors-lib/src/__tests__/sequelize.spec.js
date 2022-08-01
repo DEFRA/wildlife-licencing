78274f23aeb6fa4686ce1c0fcba36869b7769b5d
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2022-08-01 15:47:44 +0100

    Removed unused secrets manager

--- packages/connectors-lib/src/__tests__/sequelize.spec.js
+++ packages/connectors-lib/src/__tests__/sequelize.spec.js
@@ -1,10 +1,3 @@
-jest.mock('@aws-sdk/client-secrets-manager', () => {
-  return {
-    GetSecretValueCommand: jest.fn().mockImplementation(() => { return {} }),
-    SecretsManagerClient: jest.fn().mockImplementation(() => { return { send: () => ({ SecretString: 'bar' }) } })
-  }
-})
-
 describe('The sequelize connector', () => {
   it('initializes', async () => {
     const { SEQUELIZE } = require('../sequelize.js')
