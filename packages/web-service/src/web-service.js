367a3c900824eda587cb84238391a6152b6f19b6
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2022-08-03 09:47:22 +0100

    Clam fail fast

--- packages/web-service/src/web-service.js
+++ packages/web-service/src/web-service.js
@@ -1,11 +1,11 @@
 import { init, createServer } from './server.js'
+import { initializeClamScan } from './services/virus-scan.js'
 import { REDIS } from '@defra/wls-connectors-lib'
 
 REDIS.initialiseConnection()
-  .then(() => createServer()
-    .then(s =>
-      init(s).catch(e => {
+  .then(() => initializeClamScan()
+    .then(() => createServer()
+      .then(s => init(s).catch(e => {
         console.error(e)
         process.exit(1)
-      })
-    ))
+      }))))
