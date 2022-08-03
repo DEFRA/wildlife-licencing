824e5dd5b342b49a0a54c52a45fb67c3f4041551
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2022-08-03 10:03:14 +0100

    Coverage on new code

--- packages/web-service/src/services/__tests__/virus-scan.spec.js
+++ packages/web-service/src/services/__tests__/virus-scan.spec.js
@@ -1,9 +1,38 @@
 
 describe('The virus scanning service', () => {
   beforeEach(() => jest.resetModules())
+  describe('The clam initialization', () => {
+    it('resolves when initialized', async () => {
+      jest.doMock('clamscan', () => jest.fn().mockImplementation(() => ({
+        init: () => Promise.resolve()
+      })))
+      const { initializeClamScan } = await import('../virus-scan.js')
+      await expect(() => initializeClamScan()).resolves
+    })
+    it('rejects when initialization fails', async () => {
+      jest.doMock('clamscan', () => jest.fn().mockImplementation(() => ({
+        init: () => Promise.reject(new Error())
+      })))
+      const { initializeClamScan } = await import('../virus-scan.js')
+      await expect(() => initializeClamScan()).rejects.toThrow()
+    })
+  })
 
   describe('Scan file request', () => {
-    it('returns a boolean when initialised successfully', async () => {
+    it('returns a true and unlinks when scanned infected file', async () => {
+      const mockScan = { isInfected: true }
+      const mockUnlinkSync = jest.fn()
+      jest.doMock('fs', () => ({ unlinkSync: mockUnlinkSync }))
+      jest.doMock('clamscan', () => jest.fn().mockImplementation(() => ({
+        init: () => Promise.resolve({ isInfected: () => mockScan })
+      })))
+      const { scanFile, initializeClamScan } = await import('../virus-scan.js')
+      await initializeClamScan()
+      expect(await scanFile('text.txt')).toBe(true)
+      await expect(mockUnlinkSync).toHaveBeenCalledWith('text.txt')
+    })
+
+    it('returns a false when scanned clean file', async () => {
       const mockScan = { isInfected: false }
       jest.doMock('clamscan', () => jest.fn().mockImplementation(() => ({
         init: () => Promise.resolve({ isInfected: () => mockScan })
@@ -22,7 +51,7 @@ describe('The virus scanning service', () => {
       const { scanFile, initializeClamScan } = await import('../virus-scan.js')
       await initializeClamScan()
       await expect(() => scanFile('text.txt')).rejects.toThrowError()
-      await expect(mockUnlinkSync).toHaveBeenCalledTimes(1)
+      await expect(mockUnlinkSync).toHaveBeenCalledWith('text.txt')
     })
   })
 })
