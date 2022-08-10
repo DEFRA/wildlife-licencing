e63b389090b33af638c47a36d3dfe95b22e7cdbc
Parr <kealan.parr@capgemini.com>
2022-08-10 16:08:39 +0100

    fix: add joi validator for 3 more pages

--- packages/web-service/src/pages/habitat/active-entrances/habitat-active-entrances.js
+++ packages/web-service/src/pages/habitat/active-entrances/habitat-active-entrances.js
@@ -1,6 +1,14 @@
+import Joi from 'joi'
 import pageRoute from '../../../routes/page-route.js'
 import { habitatURIs } from '../../../uris.js'
 
 export const completion = async _request => habitatURIs.GRID_REF.uri
 
-export default pageRoute({ page: habitatURIs.ACTIVE_ENTRANCES.page, uri: habitatURIs.ACTIVE_ENTRANCES.uri, completion })
+export default pageRoute({
+  page: habitatURIs.ACTIVE_ENTRANCES.page,
+  uri: habitatURIs.ACTIVE_ENTRANCES.uri,
+  validator: Joi.object({
+    'habitat-active-entrances': Joi.number().integer().required()
+  }).options({ abortEarly: false, allowUnknown: true }),
+  completion
+})
