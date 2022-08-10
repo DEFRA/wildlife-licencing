a2fb83bb0046bc47b5738d5fb2c5f13d660b6369
Parr <kealan.parr@capgemini.com>
2022-08-10 10:57:36 +0100

    fix: wire up joi validation for habitat name page

--- packages/web-service/src/pages/habitat/name/habitat-name.js
+++ packages/web-service/src/pages/habitat/name/habitat-name.js
@@ -1,6 +1,22 @@
+import Joi from 'joi'
 import pageRoute from '../../../routes/page-route.js'
 import { habitatURIs } from '../../../uris.js'
 
 export const completion = async _request => habitatURIs.TYPES.uri
 
-export default pageRoute({ page: habitatURIs.NAME.page, uri: habitatURIs.NAME.uri, completion })
+export const validator = async payload => {
+  if (payload['habitat-name'] === '') {
+    throw new Joi.ValidationError('ValidationError', [{
+      message: 'Error: no habitat name has been sent',
+      path: ['habitat-name'],
+      type: 'no-name-entered',
+      context: {
+        label: 'habitat-name',
+        value: 'Error',
+        key: 'habitat-name'
+      }
+    }], null)
+  }
+}
+
+export default pageRoute({ page: habitatURIs.NAME.page, uri: habitatURIs.NAME.uri, completion, validator })
