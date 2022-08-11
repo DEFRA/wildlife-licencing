9b93a0260133059a1e9816e4879c0b921564bf15
Parr <kealan.parr@capgemini.com>
2022-08-11 12:57:57 +0100

    fix: ensure good test coverage and add new line

--- packages/web-service/src/pages/habitat/grid-ref/__tests__/habitat-grid-ref.spec.js
+++ packages/web-service/src/pages/habitat/grid-ref/__tests__/habitat-grid-ref.spec.js
@@ -7,4 +7,49 @@ describe('The habitat grid ref page', () => {
       expect(await completion()).toBe('/habitat-work-start')
     })
   })
+
+  it('the habitat-grid-ref page validates an empty string correctly', async () => {
+    const { validator } = await import('../habitat-grid-ref.js')
+    try {
+      await validator({ 'habitat-grid-ref': '' })
+    } catch (e) {
+      expect(e.message).toBe('ValidationError')
+      expect(e.details[0].message).toBe('Error: no grid ref has been sent')
+    }
+  })
+
+  it('the habitat-grid-ref page validates an invalid grid ref (of strings) correctly', async () => {
+    const { validator } = await import('../habitat-grid-ref.js')
+    try {
+      await validator({ 'habitat-grid-ref': 'aaaaaaaa' })
+    } catch (e) {
+      expect(e.message).toBe('ValidationError')
+      expect(e.details[0].message).toBe('Error: grid ref is invalid')
+    }
+  })
+
+  it('the habitat-grid-ref page validates an invalid grid ref (of numbers) correctly', async () => {
+    const { validator } = await import('../habitat-grid-ref.js')
+    try {
+      await validator({ 'habitat-grid-ref': '12121212' })
+    } catch (e) {
+      expect(e.message).toBe('ValidationError')
+      expect(e.details[0].message).toBe('Error: grid ref is invalid')
+    }
+  })
+
+  it('the habitat-grid-ref page validates a wrong length grid-ref correctly', async () => {
+    const { validator } = await import('../habitat-grid-ref.js')
+    try {
+      await validator({ 'habitat-grid-ref': 'NY39555' })
+    } catch (e) {
+      expect(e.message).toBe('ValidationError')
+      expect(e.details[0].message).toBe('Error: grid ref is invalid')
+    }
+  })
+
+  it('the habitat-grid-ref page validates a wrong length grid-ref correctly', async () => {
+    const { validator } = await import('../habitat-grid-ref.js')
+    expect(await validator({ 'habitat-grid-ref': 'NY395551' })).toBe(null)
+  })
 })
--- packages/web-service/src/pages/habitat/grid-ref/habitat-grid-ref.js
+++ packages/web-service/src/pages/habitat/grid-ref/habitat-grid-ref.js
@@ -5,28 +5,29 @@ import { habitatURIs } from '../../../uris.js'
 export const completion = async _request => habitatURIs.WORK_START.uri
 
 export const validator = async payload => {
-  const gridRef = payload['habitat-grid-ref']
+  const habitatGridKey = 'habitat-grid-ref'
+  const gridRef = payload[habitatGridKey]
 
   if (gridRef === '') {
     throw new Joi.ValidationError('ValidationError', [{
       message: 'Error: no grid ref has been sent',
-      path: ['habitat-grid-ref'],
+      path: [habitatGridKey],
       type: 'no-reference-sent',
       context: {
-        label: 'habitat-grid-ref',
+        label: habitatGridKey,
         value: 'Error',
-        key: 'habitat-grid-ref'
+        key: habitatGridKey
       }
     }], null)
   } else if (/[a-zA-Z]{2}\d{6}/.test(gridRef) === false) {
     throw new Joi.ValidationError('ValidationError', [{
       message: 'Error: grid ref is invalid',
-      path: ['habitat-grid-ref'],
+      path: [habitatGridKey],
       type: 'invalid-reference',
       context: {
-        label: 'habitat-grid-ref',
+        label: habitatGridKey,
         value: 'Error',
-        key: 'habitat-grid-ref'
+        key: habitatGridKey
       }
     }], null)
   }
