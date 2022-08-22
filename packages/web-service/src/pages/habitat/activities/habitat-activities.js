1e1822a24038d87cd4918e54dc12f42e65e20369
Kealan Parr <kealantparr@gmail.com>
2022-08-22 12:50:14 +0100

    fix: remove check-your-answers-api hit

--- packages/web-service/src/pages/habitat/activities/habitat-activities.js
+++ packages/web-service/src/pages/habitat/activities/habitat-activities.js
@@ -1,8 +1,7 @@
+import Joi from 'joi'
 import pageRoute from '../../../routes/page-route.js'
 import { habitatURIs } from '../../../uris.js'
 import { PowerPlatformKeys } from '@defra/wls-powerapps-keys'
-import Joi from 'joi'
-import { APIRequests } from '../../../services/api-requests.js'
 const { METHOD_IDS: { DAMAGE_A_SETT, DESTROY_A_SETT, DISTURB_A_SETT, OBSTRUCT_SETT_WITH_BLOCK_OR_PROOF, OBSTRUCT_SETT_WITH_GATES } } = PowerPlatformKeys
 
 export const completion = async _request => habitatURIs.CHECK_YOUR_ANSWERS.uri
@@ -15,18 +14,6 @@ export const getData = async _request => {
     DESTROY_A_SETT,
     DISTURB_A_SETT
   }
-}
-
-export const setData = async request => {
-  const pageData = await request.cache().getPageData()
-  const journeyData = await request.cache().getData()
-  const methodIds = pageData.payload['habitat-activities']
-  console.log(journeyData)
-  Object.assign(journeyData, { methodIds })
-  journeyData.applicationId = '4e65a8d3-0b47-4c09-ac2c-e2390235dde9'
-  // const newSite = await APIRequests.HABITAT.create(journeyData.applicationId, journeyData)
-  const habitatSites = await APIRequests.HABITAT.getHabitatsById(journeyData.applicationId)
-  request.cache().setData(habitatSites)
 }
 
 const validator = async payload => {
@@ -44,4 +31,4 @@ const validator = async payload => {
   }
 }
 
-export default pageRoute({ page: habitatURIs.ACTIVITIES.page, uri: habitatURIs.ACTIVITIES.uri, validator, setData, completion, getData })
+export default pageRoute({ page: habitatURIs.ACTIVITIES.page, uri: habitatURIs.ACTIVITIES.uri, validator, completion, getData })
