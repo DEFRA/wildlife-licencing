07c1da2c4b1101fd8bd1eac570772747917dbd88
sfaulkner89 <s.faulkner89@gmail.com>
2022-08-23 12:39:16 +0100

    fixed logic smells

--- packages/web-service/src/utils/invalid-date.js
+++ packages/web-service/src/utils/invalid-date.js
@@ -1,15 +1,3 @@
 export const invalidDate = (day, month, dateString) => {
-  if (day > 31 || day <= 0) {
-    return true
-  }
-
-  if (month > 12 || month <= 0) {
-    return true
-  }
-
-  if (!(/^[\d-]*$/.test(dateString))) { // Validate we just have dashes and numbers
-    return true
-  }
-
-  return false
+  return (day > 31 || day <= 0) || (month > 12 || month <= 0) || (!(/^[\d-]*$/.test(dateString)))
 }
--- packages/web-service/src/utils/is-date.js
+++ packages/web-service/src/utils/is-date.js
@@ -1,9 +1,5 @@
 export const isDate = date => {
   const isValidDate = Date.parse(date)
 
-  if (isNaN(isValidDate)) {
-    return false
-  }
-
-  return true
+  return !isNaN(isValidDate)
 }
