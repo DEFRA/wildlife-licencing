d23b7b25c6f8a9b10122a8e0c3aafd04ce252e00
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2022-10-03 08:48:13 +0100

    Site API/PP Mappings

--- packages/powerapps-lib/src/schema/tables/sdds-site.js
+++ packages/powerapps-lib/src/schema/tables/sdds-site.js
@@ -4,10 +4,24 @@ export const SddsSite = new Table('sdds_sites', [
   new Column('sdds_name', 'name'),
   new Column('sdds_sourceremote', null, () => true, null, OperationType.INBOUND_AND_OUTBOUND, () => 'sdds_sourceremote eq true'),
   new Column('sdds_osgridreference', 'gridReference'),
-  new Column('sdds_addressline1', 'address.addrline1'),
-  new Column('sdds_addressline2', 'address.addrline2'),
-  new Column('sdds_addressline3', 'address.addrline3'),
-  new Column('sdds_county', 'address.county'),
+
+  new Column('sdds_address1_subbuildingname', 'address.subBuildingName'),
+  new Column('sdds_address1_buildingname', 'address.buildingName'),
+  new Column('sdds_housenumber', 'address.buildingNumber'),
+
+  new Column('sdds_addressline1', 'address.addressLine1'),
+  new Column('sdds_addressline1', 'address.street'),
+  new Column('sdds_addressline2', 'address.addressLine2'),
+  new Column('sdds_addressline2', 'address.locality'),
+  new Column('sdds_addressline3', 'address.addressLine3'),
+  new Column('sdds_addressline3', 'address.dependentLocality'),
   new Column('sdds_town', 'address.town'),
-  new Column('sdds_postcode', 'address.postcode', s => s.toUpperCase())
+  new Column('sdds_postcode', 'address.postcode', s => s.toUpperCase()),
+
+  new Column('sdds_address1_xcoordinate', 'address.xCoordinate'),
+  new Column('sdds_address1_ycoordinate', 'address.yCoordinate'),
+  new Column('sdds_address1_uprn', 'address.uprn'),
+  new Column('sdds_county', 'address.county'),
+
+  new Column('sdds_address1_country', 'address.country')
 ], [], 'sites', 'sites', 'sdds_siteid')
