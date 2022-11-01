7f7272c56050c85ef41fc1a6e29fe6b9bcfae7b2
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2022-11-01 08:09:26 +0000

    Remove unnecessary try-catch from auth

--- packages/api/src/handlers/user/password.js
+++ packages/api/src/handlers/user/password.js
@@ -1,21 +1,4 @@
 import argon2 from 'argon2'
 
-export const toHash = async plaintext => {
-  try {
-    return argon2.hash(plaintext, {
-      type: argon2.argon2id
-    })
-  } catch (err) {
-    console.error(err)
-    return false
-  }
-}
-
-export const verify = async (plaintext, hash) => {
-  try {
-    return argon2.verify(hash, plaintext)
-  } catch (err) {
-    console.error(err)
-    return false
-  }
-}
+export const toHash = async plaintext => argon2.hash(plaintext, { type: argon2.argon2id })
+export const verify = async (plaintext, hash) => argon2.verify(hash, plaintext)
