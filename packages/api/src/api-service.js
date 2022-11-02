3eab5f10fe9e56a288e3ae3c2c97e6f2b5f3fa31
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2022-11-02 14:16:38 +0000

    Print name and version from the package into the logfile

--- packages/api/src/api-service.js
+++ packages/api/src/api-service.js
@@ -3,6 +3,7 @@ import { init, createServer } from './server.js'
 import { SEQUELIZE, REDIS } from '@defra/wls-connectors-lib'
 import { createQueue, queueDefinitions } from '@defra/wls-queue-defs'
 import { createModels } from '@defra/wls-database-model'
+import fs from 'fs'
 
 const initialize = async () => {
   await SEQUELIZE.initialiseConnection()
@@ -16,6 +17,8 @@ const debug = db('api:env')
 
 // Warning -- may print sensitive info. Ensure disabled in production
 debug(`Environment: ${JSON.stringify(process.env, null, 4)}`)
+const json = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
+console.log(`Starting ${json.name}:${json.version}`)
 
 initialize()
   .then(() => createServer()
--- packages/file-queue-processor/src/file-queue-processor.js
+++ packages/file-queue-processor/src/file-queue-processor.js
@@ -2,6 +2,10 @@ import { SEQUELIZE, GRAPH } from '@defra/wls-connectors-lib'
 import { createQueue, queueDefinitions, queueWorker } from '@defra/wls-queue-defs'
 import { createModels } from '@defra/wls-database-model'
 import { fileJobProcess } from './file-job-process.js'
+import fs from 'fs'
+
+const json = JSON.parse(fs.readFileSync('./package.json', 'utf8'))
+console.log(`Starting ${json.name}:${json.version}`)
 
 Promise.all([
   SEQUELIZE.initialiseConnection().then(() => createModels()),
