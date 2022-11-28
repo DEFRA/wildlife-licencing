17dafe48611756c0e335357c074aa3c5669f879b
Berhane Yohannes <berhane.yohannes@capgemini.com>
2022-11-28 14:22:36 +0000

    fix: Site information - continue on postcode is displaying undefined address instead of erroring

--- packages/web-service/src/pages/site/site-got-postcode/site-got-postcode.js
+++ packages/web-service/src/pages/site/site-got-postcode/site-got-postcode.js
@@ -14,7 +14,7 @@ export const validator = async payload => {
     }).options({ abortEarly: false, allowUnknown: true }))
   }
 
-  if (payload[postcodeInput] && payload[postcodeRadio] === 'yes') {
+  if ((!payload[postcodeInput] || payload[postcodeInput]) && payload[postcodeRadio] === 'yes') {
     Joi.assert(payload, Joi.object({
       'site-postcode': Joi.string().trim().required().pattern(ukPostcodeRegex).replace(ukPostcodeRegex, '$1 $2').uppercase()
     }).options({ abortEarly: false, allowUnknown: true }))
