9ab5e3ba3ebb2af22cbc1ec5a904d8bda229f05b
Berhane Yohannes <berhane.yohannes@capgemini.com>
2022-12-12 11:37:38 +0000

    Ensure unique previous mitigation licence details are saved

--- packages/web-service/src/pages/ecologist-experience/enter-licence-details/__tests__/enter-licence-details.spec.js
+++ packages/web-service/src/pages/ecologist-experience/enter-licence-details/__tests__/enter-licence-details.spec.js
@@ -1,36 +1,88 @@
 describe('The enter licence details page', () => {
   beforeEach(() => jest.resetModules())
 
-  describe('completion function', () => {
-    it('returns the licence uri', async () => {
-      const { completion } = await import('../enter-licence-details.js')
-      expect(await completion()).toBe('/licence')
-    })
-  })
-
-  describe('setData function', () => {
-    it('sets the licence details in the api', async () => {
-      const mockAdd = jest.fn()
-      jest.doMock('../../../../services/api-requests.js', () => ({
-        APIRequests: {
-          ECOLOGIST_EXPERIENCE: {
-            addPreviousLicence: mockAdd
-          }
-        }
-      }))
-      const request = {
-        payload: {
-          'enter-licence-details': 'AB1234'
-        },
-        cache: () => ({
+  jest.doMock('../../../../session-cache/cache-decorator.js', () => {
+    return {
+      cacheDirect: () => {
+        return {
           getData: () => ({
             applicationId: '26a3e94f-2280-4ea5-ad72-920d53c110fc'
           })
-        })
+        }
       }
-      const { setData } = await import('../enter-licence-details.js')
-      await setData(request)
-      expect(mockAdd).toHaveBeenCalledWith('26a3e94f-2280-4ea5-ad72-920d53c110fc', 'AB1234')
-    })
+    }
+  })
+
+  it('should throw an error for an empty licence details', async () => {
+    try {
+      jest.doMock('../../../../services/api-requests.js', () => ({
+        APIRequests: {
+          ECOLOGIST_EXPERIENCE: {
+            getPreviousLicences: () => ['hello']
+          }
+        }
+      }))
+      const payload = { 'enter-licence-details': '' }
+      const { validator } = await import('../enter-licence-details.js')
+      expect(await validator(payload))
+    } catch (e) {
+      expect(e.message).toBe('ValidationError')
+      expect(e.details[0].message).toBe('Error: You have not entered a licence number')
+    }
+  })
+
+  it('should throw an error for an existing licence details', async () => {
+    try {
+      jest.doMock('../../../../services/api-requests.js', () => ({
+        APIRequests: {
+          ECOLOGIST_EXPERIENCE: {
+            getPreviousLicences: () => ['licence1']
+          }
+        }
+      }))
+      const payload = { 'enter-licence-details': 'licence1' }
+      const { validator } = await import('../enter-licence-details.js')
+      expect(await validator(payload))
+    } catch (e) {
+      expect(e.message).toBe('ValidationError')
+      expect(e.details[0].message).toBe('Error: You have already entered this badger mitigation licence')
+    }
+  })
+
+  it('should not throw an error for a valid licence details', async () => {
+    jest.doMock('../../../../services/api-requests.js', () => ({
+      APIRequests: {
+        ECOLOGIST_EXPERIENCE: {
+          getPreviousLicences: () => []
+        }
+      }
+    }))
+    const payload = { 'enter-licence-details': 'licence2' }
+    const { validator } = await import('../enter-licence-details.js')
+    expect(await validator(payload)).toBeUndefined()
+  })
+
+  it('sets the licence details in the api', async () => {
+    const mockAdd = jest.fn()
+    jest.doMock('../../../../services/api-requests.js', () => ({
+      APIRequests: {
+        ECOLOGIST_EXPERIENCE: {
+          addPreviousLicence: mockAdd
+        }
+      }
+    }))
+    const request = {
+      payload: {
+        'enter-licence-details': 'AB1234'
+      },
+      cache: () => ({
+        getData: () => ({
+          applicationId: '26a3e94f-2280-4ea5-ad72-920d53c110fc'
+        })
+      })
+    }
+    const { setData } = await import('../enter-licence-details.js')
+    await setData(request)
+    expect(mockAdd).toHaveBeenCalledWith('26a3e94f-2280-4ea5-ad72-920d53c110fc', 'AB1234')
   })
 })
--- packages/web-service/src/pages/ecologist-experience/enter-licence-details/enter-licence-details.js
+++ packages/web-service/src/pages/ecologist-experience/enter-licence-details/enter-licence-details.js
@@ -3,21 +3,50 @@ import pageRoute from '../../../routes/page-route.js'
 import { ecologistExperienceURIs } from '../../../uris.js'
 import { APIRequests } from '../../../services/api-requests.js'
 import { checkApplication } from '../../common/check-application.js'
+import { cacheDirect } from '../../../session-cache/cache-decorator.js'
 
-export const completion = async () => ecologistExperienceURIs.LICENCE.uri
+const licenceDetailsInput = 'enter-licence-details'
+
+export const validator = async (payload, context) => {
+  const { applicationId } = await cacheDirect(context).getData()
+  const previousLicences = await APIRequests.ECOLOGIST_EXPERIENCE.getPreviousLicences(applicationId)
+  const found = previousLicences.find(previousLicence => previousLicence.trim() === payload[licenceDetailsInput].trim())
+  if (!payload[licenceDetailsInput]) {
+    throw new Joi.ValidationError('ValidationError', [{
+      message: 'Error: You have not entered a licence number',
+      path: [licenceDetailsInput],
+      type: 'emptyLicenceDetails',
+      context: {
+        label: licenceDetailsInput,
+        value: 'Error',
+        key: licenceDetailsInput
+      }
+    }], null)
+  }
+  if (found) {
+    throw new Joi.ValidationError('ValidationError', [{
+      message: 'Error: You have already entered this badger mitigation licence',
+      path: [licenceDetailsInput],
+      type: 'existingLicenceDetails',
+      context: {
+        label: licenceDetailsInput,
+        value: 'Error',
+        key: licenceDetailsInput
+      }
+    }], null)
+  }
+}
 
 export const setData = async request => {
   const { applicationId } = await request.cache().getData()
-  await APIRequests.ECOLOGIST_EXPERIENCE.addPreviousLicence(applicationId, request.payload['enter-licence-details'])
+  await APIRequests.ECOLOGIST_EXPERIENCE.addPreviousLicence(applicationId, request.payload[licenceDetailsInput])
 }
 
 export default pageRoute({
   uri: ecologistExperienceURIs.ENTER_LICENCE_DETAILS.uri,
   page: ecologistExperienceURIs.ENTER_LICENCE_DETAILS.page,
   checkData: checkApplication,
-  validator: Joi.object({
-    'enter-licence-details': Joi.string().required()
-  }).options({ abortEarly: false, allowUnknown: true }),
-  setData,
-  completion
+  completion: ecologistExperienceURIs.LICENCE.uri,
+  validator,
+  setData
 })
