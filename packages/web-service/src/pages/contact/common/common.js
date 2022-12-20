299f8c5f12c7778b3f3a75723a931eeaa7bea3f5
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2022-12-20 10:32:46 +0000

    Tweak the contact candidate selection

--- packages/web-service/src/pages/contact/common/common.js
+++ packages/web-service/src/pages/contact/common/common.js
@@ -4,11 +4,16 @@ const getContactCandidatesInner = async (primaryContactRole, otherContactRoles, 
   const allRoles = [primaryContactRole].concat(otherContactRoles)
   const contactApplications = await APIRequests.CONTACT.findAllContactApplicationRolesByUser(userId)
 
-  // Filter by roles. If the primary (choosing) role is APPLICANT can select
-  // APPLICANTS and ALTERNATIVE APPLICANTS from other applications
-  // When selecting the PAYER, the PAYER role can be from the current application ALTERNATIVE-APPLICANT ROLE.
-  // i.e. the contact roles are mutually exclusive within the same application except for the payer,
-  // which is handled by explicit selection, not by the candidate generator.
+  /*
+    Filter by roles. If the primary (choosing) role is APPLICANT, then the candidates are the
+    APPLICANTS and ALTERNATIVE APPLICANTS from other applications.
+    The contact roles are mutually exclusive within the same application except for the payer,
+    which, if selected as the applicant or ecologist contact is
+    handled by explicit selection, not by the candidate generator.
+    Currently, the payer does not select the alternative applicants and ecologists
+    as candidate. That would require a change here to get the immutability for same
+    application-different role scenario
+  */
   const contactApplicationsOfRoles = contactApplications
     .filter(ca => allRoles.includes(ca.contactRole) && ca.applicationId !== applicationId)
 
@@ -26,7 +31,8 @@ const getContactCandidatesInner = async (primaryContactRole, otherContactRoles, 
       assoc: !contact.userId || allowAssociated,
       isImmutable: !contact.fullName
         ? false
-        : contact.submitted || !!contactApplications.find(ac => ac.id === contact.id && ac.applicationId !== applicationId)
+        : contact.submitted || !!contactApplications.find(ac => ac.id === contact.id &&
+        ac.applicationId !== applicationId)
     }]
   }))
 
