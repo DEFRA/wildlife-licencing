5779c702908b12482fb3fded6e8e280c4240656e
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2023-01-31 13:48:10 +0000

    Remove unused parameter

--- packages/api/src/handlers/application/application-section/delete-section.js
+++ packages/api/src/handlers/application/application-section/delete-section.js
@@ -2,7 +2,7 @@ import { models } from '@defra/wls-database-model'
 import { REDIS, SEQUELIZE } from '@defra/wls-connectors-lib'
 const { cache } = REDIS
 
-export const deleteSectionHandler = (section, removeKeyFunc) => async (context, req, h) => {
+export const deleteSectionHandler = section => async (context, req, h) => {
   try {
     const { applicationId } = context.request.params
     const result = await models.applications.findByPk(applicationId)
