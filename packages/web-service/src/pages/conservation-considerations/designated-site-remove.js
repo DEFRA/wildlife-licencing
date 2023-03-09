517a3cf120947caed77bde49346e96d9ab7932b3
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2023-03-09 10:07:45 +0000

    Added remove page

--- packages/web-service/src/pages/conservation-considerations/designated-site-remove.js
+++ packages/web-service/src/pages/conservation-considerations/designated-site-remove.js
@@ -1,30 +1,32 @@
 import { conservationConsiderationURIs } from '../../uris.js'
 import { isYes, yesNoPage } from '../common/yes-no.js'
 import { checkApplication } from '../common/check-application.js'
+import { getCurrentSite, getFilteredDesignatedSites } from './common.js'
+import { APIRequests } from '../../services/api-requests.js'
 
-const { DESIGNATED_SITE_REMOVE } = conservationConsiderationURIs
-
-export const checkData = async request => {
-  return null
-}
+const { DESIGNATED_SITE_REMOVE, DESIGNATED_SITE_CHECK_ANSWERS } = conservationConsiderationURIs
 
 export const getData = async request => {
-  return { yesNo: null }
+  const sites = await getFilteredDesignatedSites()
+  const ads = await getCurrentSite(request)
+  return { name: sites.find(s => s.id === ads.designatedSiteId).siteName }
 }
 
 export const setData = async request => {
-
-}
-
-export const completion = async request => {
-  return conservationConsiderationURIs.DESIGNATED_SITE_REMOVE.uri
+  if (isYes(request)) {
+    const journeyData = await request.cache().getData()
+    const { applicationId, designatedSite } = journeyData
+    await APIRequests.DESIGNATED_SITES.destroy(applicationId, designatedSite.id)
+    delete journeyData.designatedSite
+    await request.cache().setData(journeyData)
+  }
 }
 
 export const designatedSiteRemove = yesNoPage({
   page: DESIGNATED_SITE_REMOVE.page,
   uri: DESIGNATED_SITE_REMOVE.uri,
-  checkData: [checkApplication, checkData],
+  checkData: checkApplication,
   getData: getData,
-  completion: completion,
+  completion: DESIGNATED_SITE_CHECK_ANSWERS.uri,
   setData: setData
 })
