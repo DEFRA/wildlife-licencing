c203745ef7d2805207395278ea7b5ac6ccddc36b
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2023-03-09 13:52:34 +0000

    More coverage

--- packages/web-service/src/pages/conservation-considerations/__tests__/designated-site-permission.spec.js
+++ packages/web-service/src/pages/conservation-considerations/__tests__/designated-site-permission.spec.js
@@ -35,7 +35,7 @@ describe('the designated site permission functions', () => {
   })
 
   describe('the setData function', () => {
-    it('sets the permissionFromOwner flas', async () => {
+    it('sets the permissionFromOwner flag if \'yes\'', async () => {
       const mockUpdate = jest.fn()
       jest.doMock('../../../services/api-requests.js', () => ({
         APIRequests: {
@@ -71,6 +71,45 @@ describe('the designated site permission functions', () => {
           designatedSiteType: 100000001,
           id: '344be97d-c928-4753-ae09-f8944ad9f228',
           permissionFromOwner: true
+        })
+    })
+
+    it('sets the permissionFromOwner flag if \'no\'', async () => {
+      const mockUpdate = jest.fn()
+      jest.doMock('../../../services/api-requests.js', () => ({
+        APIRequests: {
+          DESIGNATED_SITES: {
+            get: jest.fn(() => [{
+              id: '344be97d-c928-4753-ae09-f8944ad9f228',
+              designatedSiteId: 'fa5b8103-56a9-ed11-aad1-0022481b53bf',
+              designatedSiteType: 100000001,
+              permissionFromOwner: true
+            }]),
+            update: mockUpdate
+          }
+        }
+      }))
+      const request = {
+        payload: { 'yes-no': 'no' },
+        cache: () => ({
+          getData: () => ({
+            applicationId: '26a3e94f-2280-4ea5-ad72-920d53c110fc',
+            designatedSite: {
+              designatedSiteId: 'fa5b8103-56a9-ed11-aad1-0022481b53bf',
+              id: '344be97d-c928-4753-ae09-f8944ad9f228'
+            }
+          })
+        })
+      }
+      const { setData } = await import('../designated-site-permission.js')
+      await setData(request)
+      expect(mockUpdate).toHaveBeenCalledWith('26a3e94f-2280-4ea5-ad72-920d53c110fc',
+        '344be97d-c928-4753-ae09-f8944ad9f228',
+        {
+          designatedSiteId: 'fa5b8103-56a9-ed11-aad1-0022481b53bf',
+          designatedSiteType: 100000001,
+          id: '344be97d-c928-4753-ae09-f8944ad9f228',
+          permissionFromOwner: false
         })
     })
   })
