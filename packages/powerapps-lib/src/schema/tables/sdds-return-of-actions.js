a9a12a532edd692a109c38dcf590a85656dfa51a
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2023-03-17 11:56:04 +0000

    Coverage tweaks

--- packages/powerapps-lib/src/schema/tables/sdds-return-of-actions.js
+++ packages/powerapps-lib/src/schema/tables/sdds-return-of-actions.js
@@ -3,12 +3,12 @@ import { yesNoNASrc, yesNoNATgt } from './common.js'
 
 export const SddsReturn = new Table('sdds_returnofactions', [
   new Column('sdds_nilroa', 'nilReturn'),
-  new Column('sdds_didyoucompleteworkbetweenlicenseddates', 'completedWithinLicenceDates', s => yesNoNASrc(s), t => yesNoNATgt(t)),
+  new Column('sdds_didyoucompleteworkbetweenlicenseddates', 'completedWithinLicenceDates', yesNoNASrc, yesNoNATgt),
   new Column('sdds_whendidyoustartwork', 'startDate'),
   new Column('sdds_whendidyoucompletethework', 'endDate'),
   new Column('sdds_whywasworknotcompletebetweenlicenseddates', 'whyNotCompletedWithinLicenceDates'),
-  new Column('outcome', 'sdds_developmentcouldstart', s => yesNoNASrc(s), t => yesNoNATgt(t)),
-  new Column('sdds_damagesettbyhandsmechanicalmeans', 'destroyVacantSettByMechanicalMeans', s => yesNoNASrc(s), t => yesNoNATgt(t))
+  new Column('outcome', 'sdds_developmentcouldstart', yesNoNASrc, yesNoNATgt),
+  new Column('sdds_damagesettbyhandsmechanicalmeans', 'destroyVacantSettByMechanicalMeans', yesNoNASrc, yesNoNATgt)
 ], [
   new Relationship('sdds_returnofaction_sdds_licenceid_sdds_l', 'sdds_licenses',
     RelationshipType.MANY_TO_ONE, 'sdds_licenceid',
