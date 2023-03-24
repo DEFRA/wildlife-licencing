086cedb1475b6023b973965b3d2871f938f6a9de
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2023-03-24 13:53:12 +0000

    Fix start and end date bug

--- packages/api/src/handlers/licence/licence-proc.js
+++ packages/api/src/handlers/licence/licence-proc.js
@@ -2,6 +2,6 @@ export const prepareResponse = a => (({ licence, ...l }) => l)(Object.assign(a, 
   ...a.licence,
   createdAt: a.createdAt.toISOString(),
   updatedAt: a.updatedAt.toISOString(),
-  startDate: (new Date(a.licence.startDate)).toISOString().substring(0, 10),
-  endDate: (new Date(a.licence.endDate)).toISOString().substring(0, 10)
+  startDate: a.licence.startDate && (new Date(a.licence.startDate)).toISOString().substring(0, 10),
+  endDate: a.licence.endDate && (new Date(a.licence.endDate)).toISOString().substring(0, 10)
 }))
