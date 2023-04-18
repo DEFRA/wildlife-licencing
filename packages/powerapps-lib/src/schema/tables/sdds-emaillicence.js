841cb136d38f0e6f3c28ae693fe781f81d3dcfb1
kea parr <keaparr@ip-192-168-0-21.eu-west-2.compute.internal>
2023-04-18 14:28:00 +0100

    Revert "Revert "Change resend functionality""
    This reverts commit e4c123eda864b4a4981b1c6c74ff6ca40504850b.

--- packages/powerapps-lib/src/schema/tables/sdds-emaillicence.js
+++ packages/powerapps-lib/src/schema/tables/sdds-emaillicence.js
@@ -1,16 +1,10 @@
-import { OperationType, Relationship, RelationshipType, Table } from '../schema.js'
+import { v4 as uuidv4 } from 'uuid'
+import { Column, OperationType, Relationship, RelationshipType, Table } from '../schema.js'
 
 export const SddsEmailLicence = new Table('sdds_emaillicences', [
+  new Column('sdds_name', null, () => uuidv4())
 ], [
   new Relationship('sdds_emaillicence_sdds_applicationid_sdds', 'sdds_applications',
     RelationshipType.MANY_TO_ONE, 'sdds_applicationid', 'sddsApplicationId',
-    null, null, OperationType.OUTBOUND),
-
-  new Relationship('sdds_sdds_emaillicence_contactid_contact', 'contacts',
-    RelationshipType.MANY_TO_ONE, 'sdds_contactid', 'sddsContactId',
-    null, null, OperationType.OUTBOUND),
-
-  new Relationship('sdds_sdds_emaillicence_organisationid_account', 'accounts',
-    RelationshipType.MANY_TO_ONE, 'sdds_organisationid', 'sddsAccountId',
     null, null, OperationType.OUTBOUND)
 ], 'emailLicence')
