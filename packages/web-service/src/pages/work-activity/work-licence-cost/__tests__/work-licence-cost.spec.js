4b809435f9c4c97c19efa7dcd6079a60bdc45c23
kea parr <keaparr@ip-192-168-0-46.eu-west-2.compute.internal>
2023-04-26 15:24:52 +0100

    fix: ensure licence cost, only navigates to cya in valid state

--- packages/web-service/src/pages/work-activity/work-licence-cost/__tests__/work-licence-cost.spec.js
+++ packages/web-service/src/pages/work-activity/work-licence-cost/__tests__/work-licence-cost.spec.js
@@ -2,7 +2,7 @@ describe('The work-licence-cost page', () => {
   beforeEach(() => jest.resetModules())
 
   describe('work-licence-cost page', () => {
-    it('the completion function sets the tag status', async () => {
+    it('the completion function sets the tag status, if the journey is all complete', async () => {
       const mockSet = jest.fn()
       const request = {
         cache: () => ({
@@ -15,6 +15,9 @@ describe('The work-licence-cost page', () => {
         },
         APIRequests: {
           APPLICATION: {
+            getById: () => {
+              return {}
+            },
             tags: () => {
               return { set: mockSet }
             }
@@ -26,7 +29,7 @@ describe('The work-licence-cost page', () => {
       expect(mockSet).toHaveBeenCalledWith({ tag: 'work-activity', tagState: 'complete-not-confirmed' })
     })
 
-    it('the completion function returns the CYA uri', async () => {
+    it('the completion function returns the CYA uri, if the journey is all complete', async () => {
       const request = {
         cache: () => ({
           getData: () => ({ applicationId: '123abc' })
@@ -38,6 +41,9 @@ describe('The work-licence-cost page', () => {
         },
         APIRequests: {
           APPLICATION: {
+            getById: () => {
+              return {}
+            },
             tags: () => {
               return { set: jest.fn() }
             }
@@ -46,6 +52,34 @@ describe('The work-licence-cost page', () => {
       }))
       const { completion } = await import('../work-licence-cost.js')
       expect(await completion(request)).toEqual('/check-work-answers')
+    })
+
+    it('the completion function returns the WORK_PROPOSAL uri, if part of the journey is skipped', async () => {
+      const request = {
+        cache: () => ({
+          getData: () => ({ applicationId: '123abc' })
+        })
+      }
+      jest.doMock('../../../../services/api-requests.js', () => ({
+        tagStatus: {
+          COMPLETE_NOT_CONFIRMED: 'complete-not-confirmed'
+        },
+        APIRequests: {
+          APPLICATION: {
+            getById: () => {
+              return {
+                exemptFromPayment: true,
+                paymentExemptReason: undefined
+              }
+            },
+            tags: () => {
+              return { set: jest.fn() }
+            }
+          }
+        }
+      }))
+      const { completion } = await import('../work-licence-cost.js')
+      expect(await completion(request)).toEqual('/work-proposal')
     })
   })
 })
