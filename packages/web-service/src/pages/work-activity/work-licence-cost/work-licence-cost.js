5198c5eba5515fe3e01f906a56282a1f67a1a4bf
Kealan Parr <kealantparr@gmail.com>
2023-04-27 15:45:18 +0100

    fix: ensure licence cost, only navigates to cya in valid state (#315)
    Co-authored-by: kea parr <keaparr@ip-192-168-0-46.eu-west-2.compute.internal>

--- packages/web-service/src/pages/work-activity/work-licence-cost/work-licence-cost.js
+++ packages/web-service/src/pages/work-activity/work-licence-cost/work-licence-cost.js
@@ -5,9 +5,20 @@ import { workActivityURIs } from '../../../uris.js'
 import { SECTION_TASKS } from '../../tasklist/general-sections.js'
 
 export const completion = async request => {
-  const journeyData = await request.cache().getData()
-  await APIRequests.APPLICATION.tags(journeyData.applicationId).set({ tag: SECTION_TASKS.WORK_ACTIVITY, tagState: tagStatus.COMPLETE_NOT_CONFIRMED })
-  return workActivityURIs.CHECK_YOUR_ANSWERS.uri
+  const { applicationId } = await request.cache().getData()
+  const applicationData = await APIRequests.APPLICATION.getById(applicationId)
+
+  // The users can get to CYA, change their 'work-payment-exempt-reason.js' answer
+  // Which sets their flow to be 'in-progress' and then via the back button navigate to this page
+  // And click "Continue" to complete the journey
+  // We just need a small check to ensure they have completed everything necessary
+  if (applicationData?.exemptFromPayment === true && !applicationData?.paymentExemptReason) {
+    return workActivityURIs.WORK_PROPOSAL.uri
+  } else {
+    const journeyData = await request.cache().getData()
+    await APIRequests.APPLICATION.tags(journeyData.applicationId).set({ tag: SECTION_TASKS.WORK_ACTIVITY, tagState: tagStatus.COMPLETE_NOT_CONFIRMED })
+    return workActivityURIs.CHECK_YOUR_ANSWERS.uri
+  }
 }
 
 export default pageRoute({
