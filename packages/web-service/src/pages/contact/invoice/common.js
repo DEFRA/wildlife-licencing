9d67e6a41727b7826645b5ea14d53a1864ca3476
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2023-05-09 14:27:26 +0100

    Fix problem with experience remove flag

--- packages/web-service/src/pages/contact/invoice/common.js
+++ packages/web-service/src/pages/contact/invoice/common.js
@@ -1,6 +1,5 @@
 import { APIRequests } from '../../../services/api-requests.js'
 import { AccountRoles, ContactRoles } from '../common/contact-roles.js'
-import { contactURIs } from '../../../uris.js'
 
 export const generateOutput = async request => {
   const { applicationId } = await request.cache().getData()
@@ -20,16 +19,4 @@ export const generateOutput = async request => {
       return { responsible: 'other', name: p.fullName, contact: payer, account }
     }
   })(payer)
-}
-
-export const purchaseOrderCompletion = async request => {
-  const { CHECK_ANSWERS, PURCHASE_ORDER } = contactURIs.INVOICE_PAYER
-
-  const { applicationId } = await request.cache().getData()
-  const applicationData = await APIRequests.APPLICATION.getById(applicationId)
-  if (!applicationData.referenceOrPurchaseOrderNumber) {
-    return PURCHASE_ORDER.uri
-  } else {
-    return CHECK_ANSWERS.uri
-  }
 }
--- packages/web-service/src/pages/contact/invoice/invoice-address.js
+++ packages/web-service/src/pages/contact/invoice/invoice-address.js
@@ -5,9 +5,8 @@ import { getAddressData, setAddressData } from '../common/address/address.js'
 import { AccountRoles, ContactRoles } from '../common/contact-roles.js'
 import { checkHasAddress, checkHasContact } from '../common/common-handler.js'
 import { checkApplication } from '../../common/check-application.js'
-import { purchaseOrderCompletion } from './common.js'
 
-const { ADDRESS, RESPONSIBLE } = contactURIs.INVOICE_PAYER
+const { ADDRESS, RESPONSIBLE, CHECK_ANSWERS } = contactURIs.INVOICE_PAYER
 
 export const invoiceAddress = addressPage({
   page: ADDRESS.page,
@@ -15,5 +14,5 @@ export const invoiceAddress = addressPage({
   checkData: [checkApplication, checkHasContact(ContactRoles.PAYER, RESPONSIBLE), checkHasAddress(contactURIs.INVOICE_PAYER)],
   getData: getAddressData(ContactRoles.PAYER, AccountRoles.PAYER_ORGANISATION, contactURIs.INVOICE_PAYER),
   setData: setAddressData(ContactRoles.PAYER, AccountRoles.PAYER_ORGANISATION, contactURIs.INVOICE_PAYER),
-  completion: purchaseOrderCompletion
+  completion: CHECK_ANSWERS.uri
 })
