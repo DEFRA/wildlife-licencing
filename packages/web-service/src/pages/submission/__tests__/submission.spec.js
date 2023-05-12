5a06919bfe12b5792589d8d5c365d64355683a46
kea parr <keaparr@ip-192-168-1-22.eu-west-2.compute.internal>
2023-05-12 16:10:22 +0100

    fix: test coverage raise

--- packages/web-service/src/pages/submission/__tests__/submission.spec.js
+++ packages/web-service/src/pages/submission/__tests__/submission.spec.js
@@ -40,4 +40,65 @@ describe('submission spec', () => {
       expect(mockRedirect).toHaveBeenCalledWith('/tasklist')
     })
   })
+
+  it('the getData should return the current licence', async () => {
+    const request = {
+      cache: () => ({
+        getData: () => ({
+          applicationId: '35a6c59e-0faf-438b-b4d5-6967d8d075cb',
+          userId: '123abc-userid'
+        })
+      })
+    }
+    jest.doMock('../../../services/api-requests.js', () => ({
+      APIRequests: {
+        APPLICATION: {
+          findByUser: () => [{ id: '35a6c59e-0faf-438b-b4d5-6967d8d075cb', applicationReferenceNumber: '123abc' }]
+        }
+      }
+    }))
+
+    const { getData } = await import('../submission.js')
+    expect(await getData(request)).toEqual({
+      currentApplication: {
+        id: '35a6c59e-0faf-438b-b4d5-6967d8d075cb',
+        applicationReferenceNumber: '123abc'
+      }
+    })
+  })
+
+  it('the getData function calls findByUser', async () => {
+    const mockCall = jest.fn().mockImplementation(() => {
+      return [
+        {
+          id: '35a6c59e-0faf-438b-b4d5-6967d8d075cb',
+          applicationReferenceNumber: '123abc'
+        },
+        {
+          id: '35a6c59e-0faf-438b-b4d5',
+          applicationReferenceNumber: '456zxc'
+        }
+      ]
+    })
+
+    const request = {
+      cache: () => ({
+        getData: () => ({
+          applicationId: '35a6c59e-0faf-438b-b4d5-6967d8d075cb',
+          userId: '123abc-userid'
+        })
+      })
+    }
+    jest.doMock('../../../services/api-requests.js', () => ({
+      APIRequests: {
+        APPLICATION: {
+          findByUser: mockCall
+        }
+      }
+    }))
+
+    const { getData } = await import('../submission.js')
+    await getData(request)
+    expect(mockCall).toHaveBeenCalledWith('123abc-userid')
+  })
 })
