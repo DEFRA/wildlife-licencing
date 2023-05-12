1273407f137099997cbcddc78aa860090791314c
kea parr <keaparr@ip-192-168-1-22.eu-west-2.compute.internal>
2023-05-12 15:09:56 +0100

    fix: resolve submission differences

--- packages/web-service/src/pages/submission/submission.js
+++ packages/web-service/src/pages/submission/submission.js
@@ -2,6 +2,8 @@ import pageRoute from '../../routes/page-route.js'
 import { SUBMISSION, APPLICATIONS, TASKLIST } from '../../uris.js'
 import { Backlink } from '../../handlers/backlink.js'
 import { isAppSubmittable } from '../tasklist/licence-type.js'
+import { APIRequests } from '../../services/api-requests.js'
+import { checkApplication } from '../common/check-application.js'
 
 export const checkData = async (request, h) => {
   if (!await isAppSubmittable(request)) {
@@ -11,10 +13,22 @@ export const checkData = async (request, h) => {
   return null
 }
 
+export const getData = async request => {
+  const { userId, applicationId } = await request.cache().getData()
+
+  const allApplications = await APIRequests.APPLICATION.findByUser(userId)
+  const currentApplication = allApplications.filter(applic => applic.id === applicationId)
+
+  return {
+    currentApplication: currentApplication[0]
+  }
+}
+
 export default pageRoute({
   page: SUBMISSION.page,
   uri: SUBMISSION.uri,
   completion: APPLICATIONS.uri,
   backlink: Backlink.NO_BACKLINK,
-  checkData
+  checkData: [checkData, checkApplication],
+  getData
 })
