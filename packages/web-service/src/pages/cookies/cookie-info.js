9823b4bf1e6ffa52529c411d76a87fba77e78e72
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2023-05-17 09:46:52 +0100

    Revert these cookie files -- looks like they got undone in a commit

--- packages/web-service/src/pages/cookies/cookie-info.js
+++ packages/web-service/src/pages/cookies/cookie-info.js
@@ -1,7 +1,46 @@
-import pageRoute from '../../routes/page-route.js'
 import { COOKIE_INFO } from '../../uris.js'
+import { isYes, yesNoPage } from '../common/yes-no.js'
+import { APIRequests } from '../../services/api-requests.js'
+import { yesNoFromBool } from '../common/common.js'
 
-export default pageRoute({
+export const getData = async request => {
+  const journeyData = await request.cache().getData() || {}
+  const referrer = new URL(request.info.referrer)
+  journeyData.cookiesReferrer = referrer.pathname
+  await request.cache().setData(journeyData)
+  if (journeyData.userId) {
+    const user = await APIRequests.USER.getById(journeyData.userId)
+    return { yesNo: yesNoFromBool(user?.cookiePrefs?.analytics) }
+  } else if (journeyData.cookies) {
+    return { yesNo: yesNoFromBool(journeyData?.cookies?.analytics) }
+  }
+  return null
+}
+
+export const completion = async request => {
+  // Return to the calling page
+  const journeyData = await request.cache().getData()
+  return journeyData.cookiesReferrer
+}
+
+export const setData = async request => {
+  const journeyData = await request.cache().getData() || {}
+  journeyData.cookies = { analytics: isYes(request) }
+  await request.cache().setData(journeyData)
+
+  // If signed save the cookie preferences
+  if (journeyData.userId) {
+    const user = await APIRequests.USER.getById(journeyData.userId)
+    Object.assign(user, { cookiePrefs: journeyData.cookies })
+    await APIRequests.USER.update(journeyData.userId, user)
+  }
+}
+
+export default yesNoPage({
   page: COOKIE_INFO.page,
-  uri: COOKIE_INFO.uri
+  uri: COOKIE_INFO.uri,
+  getData: getData,
+  setData: setData,
+  completion: completion,
+  options: { auth: { mode: 'optional' } }
 })
