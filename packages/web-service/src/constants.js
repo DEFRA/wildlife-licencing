a340bb61297f8cd3734fcbf9b77cd568c46dc170
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2023-05-19 14:43:36 +0100

    Fix some file problems

--- packages/web-service/src/constants.js
+++ packages/web-service/src/constants.js
@@ -1,5 +1,5 @@
 export const SESSION_TTL_MS_DEFAULT = 3 * 60 * 60 * 1000
 export const SESSION_COOKIE_NAME_DEFAULT = 'sid'
 export const DEFAULT_ROLE = 'USER' // Will be replaced
-export const MAX_FILE_UPLOAD_SIZE_MB = (process.env.MAX_FILE_UPLOAD_SIZE_MB || 3) * 1024 * 1024
+export const MAX_FILE_UPLOAD_SIZE_MB = (process.env.MAX_FILE_UPLOAD_SIZE_MB || 30) * 1024 * 1024
 export const TIMEOUT_MS = 10000
