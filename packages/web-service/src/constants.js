7da3f2fd1150dbf5bb12212189fab4cae64cdc6e
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2023-05-22 10:22:06 +0100

    Trial and error

--- packages/web-service/src/constants.js
+++ packages/web-service/src/constants.js
@@ -1,5 +1,5 @@
 export const SESSION_TTL_MS_DEFAULT = 3 * 60 * 60 * 1000
 export const SESSION_COOKIE_NAME_DEFAULT = 'sid'
 export const DEFAULT_ROLE = 'USER' // Will be replaced
-export const MAX_FILE_UPLOAD_SIZE_BYTES = parseInt(process.env.MAX_FILE_UPLOAD_SIZE_MB + 1 || 31) * 1024 * 1024
+export const MAX_FILE_UPLOAD_SIZE_BYTES = (parseInt(process.env.MAX_FILE_UPLOAD_SIZE_MB || 30) + 1) * 1024 * 1024
 export const TIMEOUT_MS = (process.env.MAX_FILE_UPLOAD_TIMEOUT_MS || 60000)
