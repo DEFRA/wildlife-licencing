bb11bd0a9a7d9436f72635504ad9fa11e24a3c75
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2023-05-22 08:14:16 +0100

    Increase timeout on file upload to 1 minute and allow for future envvar

--- packages/web-service/src/constants.js
+++ packages/web-service/src/constants.js
@@ -2,4 +2,4 @@ export const SESSION_TTL_MS_DEFAULT = 3 * 60 * 60 * 1000
 export const SESSION_COOKIE_NAME_DEFAULT = 'sid'
 export const DEFAULT_ROLE = 'USER' // Will be replaced
 export const MAX_FILE_UPLOAD_SIZE_MB = (process.env.MAX_FILE_UPLOAD_SIZE_MB || 30) * 1024 * 1024
-export const TIMEOUT_MS = 10000
+export const TIMEOUT_MS = (process.env.MAX_FILE_UPLOAD_TIMEOUT_MS || 60000)
