d5d66e80ba633eb1300b19fd1c84afcfb8e5d3e3
GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>
2023-05-22 11:33:12 +0100

    Try server timeout instead

--- packages/web-service/src/constants.js
+++ packages/web-service/src/constants.js
@@ -2,6 +2,6 @@ export const SESSION_TTL_MS_DEFAULT = 3 * 60 * 60 * 1000
 export const SESSION_COOKIE_NAME_DEFAULT = 'sid'
 export const DEFAULT_ROLE = 'USER' // Will be replaced
 export const MAX_FILE_UPLOAD_SIZE_BYTES = parseInt(process.env.MAX_FILE_UPLOAD_SIZE_MB || 30) * 1024 * 1024
-export const FILE_UPLOAD_HEADROOM_BYTES = parseInt(process.env.FILE_UPLOAD_HEADROOM_BYTES || 10) * 1024 * 1024
+export const FILE_UPLOAD_HEADROOM_BYTES = parseInt(process.env.FILE_UPLOAD_HEADROOM_BYTES || 15) * 1024 * 1024
 
-export const TIMEOUT_MS = parseInt(process.env.MAX_FILE_UPLOAD_TIMEOUT_MS || 60000)
+export const TIMEOUT_MS = parseInt(process.env.MAX_FILE_UPLOAD_TIMEOUT_MS || 20000)
