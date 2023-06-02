7744a1fc36f81edfa33dfbca568cd23d77718ceb
Berhane Yohannes <berhane.yohannes@capgemini.com>
2023-06-02 03:43:54 +0100

    the actions need to be displayed dynamically according to the actions which the assessor has authorised when approving the application and granting the licence

--- packages/web-service/src/pages/returns/licensed-actions.js
+++ packages/web-service/src/pages/returns/licensed-actions.js
@@ -1,8 +1,11 @@
 import { ReturnsURIs } from '../../uris.js'
-import { isYes, yesNoPage } from '../common/yes-no.js'
+import { isYes } from '../common/yes-no.js'
+import Joi from 'joi'
 import { checkApplication } from '../common/check-application.js'
 import { APIRequests } from '../../services/api-requests.js'
 import { yesNoFromBool } from '../common/common.js'
+import pageRoute from '../../routes/page-route.js'
+import { activityTypes } from './common-return-functions.js'
 
 const { NIL_RETURN, OUTCOME, WHY_NIL } = ReturnsURIs
 
@@ -13,11 +16,13 @@ export const getData = async request => {
   journeyData.licenceId = licences[0].id
   journeyData.licenceNumber = licences[0].licenceNumber
   await request.cache().setData(journeyData)
+  const licenceActions = await APIRequests.RETURNS.getLicenceActions(licences[0].id)
+  const methodTypes = licenceActions[0]?.methodIds
   if (returnId) {
     const { nilReturn } = await APIRequests.RETURNS.getLicenceReturn(licences[0].id, returnId)
-    return { yesNo: yesNoFromBool(nilReturn) }
+    return { yesNo: yesNoFromBool(nilReturn), activityTypes, methodTypes }
   } else {
-    return { yesNo: undefined }
+    return { yesNo: undefined, activityTypes, methodTypes }
   }
 }
 
@@ -44,9 +49,14 @@ export const setData = async request => {
 
 export const completion = async request => isYes(request) ? OUTCOME.uri : WHY_NIL.uri
 
-export const licensedActions = yesNoPage({
+export default pageRoute({
   page: NIL_RETURN.page,
   uri: NIL_RETURN.uri,
+  validator: Joi.object({
+    'yes-no': Joi.any()
+      .valid('yes', 'no')
+      .required()
+  }).options({ abortEarly: false, allowUnknown: true }),
   checkData: checkApplication,
   getData: getData,
   completion: completion,
