d30b4516dff3bc2ec3835cfeadb011dcc4fd4eb5
kea parr <keaparr@keas-MBP.cust.communityfibre.co.uk>
2023-06-19 09:44:54 +0100

    fix: push tests

--- packages/web-service/src/pages/ecologist-experience/previous-licence/__tests__/previous-licence.spec.js
+++ packages/web-service/src/pages/ecologist-experience/previous-licence/__tests__/previous-licence.spec.js
@@ -280,42 +280,42 @@ describe('The previous licence page', () => {
     })
   })
 
-  // it('setData resets the journey if the user changes their answer for the previous answer question', async () => {
-  //   const mockSet = jest.fn()
-  //   jest.doMock('../../../../services/api-requests.js', () => ({
-  //     tagStatus: {
-  //       NOT_STARTED: 'not-started',
-  //       IN_PROGRESS: 'in-progress'
-  //     },
-  //     APIRequests: {
-  //       APPLICATION: {
-  //         tags: () => ({
-  //           get: () => 'complete',
-  //           set: mockSet
-  //         })
-  //       },
-  //       ECOLOGIST_EXPERIENCE: {
-  //         putExperienceById: mockSet,
-  //         getExperienceById: () => {
-  //           return {
-  //             previousLicence: false
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }))
-  //   const request = {
-  //     payload: {
-  //       'yes-no': 'yes'
-  //     },
-  //     cache: () => ({
-  //       getData: () => ({
-  //         applicationId: '26a3e94f-2280-4ea5-ad72-920d53c110fc'
-  //       })
-  //     })
-  //   }
-  //   const { setData } = await import('../previous-licence.js')
-  //   await setData(request)
-  //   expect(mockSet).toHaveBeenCalledWith({ tag: 'ecologist-experience', tagState: 'in-progress' })
-  // })
+  it('setData resets the application tags journey if the user changes their answer for the previous answer question', async () => {
+    const mockSet = jest.fn()
+    jest.doMock('../../../../services/api-requests.js', () => ({
+      tagStatus: {
+        NOT_STARTED: 'not-started',
+        IN_PROGRESS: 'in-progress'
+      },
+      APIRequests: {
+        APPLICATION: {
+          tags: () => ({
+            get: () => 'complete',
+            set: mockSet
+          })
+        },
+        ECOLOGIST_EXPERIENCE: {
+          putExperienceById: mockSet,
+          getExperienceById: () => {
+            return {
+              previousLicence: false
+            }
+          }
+        }
+      }
+    }))
+    const request = {
+      payload: {
+        'yes-no': 'yes'
+      },
+      cache: () => ({
+        getData: () => ({
+          applicationId: '26a3e94f-2280-4ea5-ad72-920d53c110fc'
+        })
+      })
+    }
+    const { setData } = await import('../previous-licence.js')
+    await setData(request)
+    expect(mockSet).toHaveBeenCalledWith({ tag: 'ecologist-experience', tagState: 'in-progress' })
+  })
 })
