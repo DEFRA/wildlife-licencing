fb866b0920839a5bb1153fdf16b2e12c2afda29d
GrahamMWillis <94051723+GrahamMWillis@users.noreply.github.com>
2023-07-21 11:07:31 +0100

    Change AWS.S3() (#415)
    Co-authored-by: GrahamMWillis <graham.willis@graham.willis@defra.gov.uk>

--- packages/connectors-lib/src/aws.js
+++ packages/connectors-lib/src/aws.js
@@ -3,44 +3,56 @@ import { S3Client, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3
 import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager'
 
 export const AWS = {
-  S3: () => {
-    const client = new S3Client({
-      ...(Config.aws.s3.endpoint && {
-        region: Config.aws.region,
-        endpoint: Config.aws.s3.endpoint,
-        forcePathStyle: true
+  S3: () => ({
+    writeFileStream: async (objectKey, fileStream) => {
+      // It should not be necessary but to try and solve the token errors reported on TEST
+      // move the client into the stream operator and destroy it immediately
+      const client = new S3Client({
+        ...(Config.aws.s3.endpoint && {
+          region: Config.aws.region,
+          endpoint: Config.aws.s3.endpoint,
+          forcePathStyle: true
+        })
       })
-    })
-    return {
-      writeFileStream: async (objectKey, fileStream) => {
-        const params = {
-          Bucket: Config.aws.s3.bucket,
-          ACL: 'authenticated-read',
-          Key: objectKey,
-          Body: fileStream
-        }
 
-        try {
-          await client.send(new PutObjectCommand(params))
-        } catch (err) {
-          console.error('Error writing stream', err)
-          throw err
-        }
-      },
-      readFileStream: async objectKey => {
-        try {
-          const response = await client.send(new GetObjectCommand({
-            Bucket: Config.aws.s3.bucket,
-            Key: objectKey
-          }))
-          return { stream: response.Body, bytes: response.ContentLength }
-        } catch (err) {
-          console.error('Error reading stream', err)
-          throw err
-        }
+      const params = {
+        Bucket: Config.aws.s3.bucket,
+        ACL: 'authenticated-read',
+        Key: objectKey,
+        Body: fileStream
+      }
+
+      try {
+        await client.send(new PutObjectCommand(params))
+      } catch (err) {
+        console.error('Error writing stream', err)
+        throw err
+      } finally {
+        client.destroy()
+      }
+    },
+    readFileStream: async objectKey => {
+      const client = new S3Client({
+        ...(Config.aws.s3.endpoint && {
+          region: Config.aws.region,
+          endpoint: Config.aws.s3.endpoint,
+          forcePathStyle: true
+        })
+      })
+      try {
+        const response = await client.send(new GetObjectCommand({
+          Bucket: Config.aws.s3.bucket,
+          Key: objectKey
+        }))
+        return { stream: response.Body, bytes: response.ContentLength }
+      } catch (err) {
+        console.error('Error reading stream', err)
+        throw err
+      } finally {
+        client.destroy()
       }
     }
-  },
+  }),
   secretsManager: () => {
     const client = new SecretsManagerClient({
       ...(Config.aws.secretsManager.endpoint && {
