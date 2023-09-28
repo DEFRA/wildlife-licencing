434ace0d1bec4f490fec45d985c54f0971e38166
Richard Mohammed <richard.mohammed@defra.gov.uk>
2023-09-28 15:29:09 +0100

    SDDSIP-1748 - Update closer to design

--- packages/web-service/src/pages/common/common.js
+++ packages/web-service/src/pages/common/common.js
@@ -3,7 +3,7 @@ export const timestampFormatter = ts => ts
   : null
 
 export const timestampFormatterWithTime = ts => ts
-  ? Intl.DateTimeFormat('en-GB', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' }).format(new Date(ts))
+  ? Intl.DateTimeFormat('en-GB', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true }).format(new Date(ts))
   : null
 
 export const yesNoFromBool = a => {
