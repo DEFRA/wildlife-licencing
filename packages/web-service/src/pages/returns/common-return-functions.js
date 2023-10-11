bc3a0e3ec4544192b29f44c159125c0a850a05db
richard-mohammed-def <141018006+richard-mohammed-def@users.noreply.github.com>
2023-10-11 15:43:09 +0100

    Update packages/web-service/src/pages/returns/common-return-functions.js

--- packages/web-service/src/pages/returns/common-return-functions.js
+++ packages/web-service/src/pages/returns/common-return-functions.js
@@ -156,4 +156,6 @@ export const redirectIfNextUriNotCheckYourAnswers = async (nextUri, h) => {
   if (nextUri !== ReturnsURIs.CHECK_YOUR_ANSWERS.uri) {
     return h.redirect(nextUri)
   }
+  
+  return null
 }
