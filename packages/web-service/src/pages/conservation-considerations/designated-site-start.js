39114d5237ded48731cffd03d9b4829f47c5764b
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2023-10-20 15:49:14 +0100

    Remove check from `designated-site-start`
    During testing we found that we don't yet have a designated site when we hit `designated-site-start`. We therefore remove `checkDesignatedSite` from this page to avoid being redirected prematurely

--- packages/web-service/src/pages/conservation-considerations/designated-site-start.js
+++ packages/web-service/src/pages/conservation-considerations/designated-site-start.js
@@ -1,13 +1,12 @@
 import pageRoute from '../../routes/page-route.js'
 import { conservationConsiderationURIs } from '../../uris.js'
 import { checkApplication } from '../common/check-application.js'
-import { checkDesignatedSite } from './common.js'
 
 const { DESIGNATED_SITE_START, DESIGNATED_SITE_NAME } = conservationConsiderationURIs
 
 export default pageRoute({
   page: DESIGNATED_SITE_START.page,
   uri: DESIGNATED_SITE_START.uri,
-  checkData: [checkApplication, checkDesignatedSite],
+  checkData: checkApplication,
   completion: DESIGNATED_SITE_NAME.uri
 })
