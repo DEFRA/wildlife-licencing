90ec23deab2c5a8f05f44853c2f54787a8dc8c0a
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2023-10-20 15:43:17 +0100

    Add `checkDesignatedSite` to relevant pages

--- packages/web-service/src/pages/conservation-considerations/designated-site-start.js
+++ packages/web-service/src/pages/conservation-considerations/designated-site-start.js
@@ -1,12 +1,13 @@
 import pageRoute from '../../routes/page-route.js'
 import { conservationConsiderationURIs } from '../../uris.js'
 import { checkApplication } from '../common/check-application.js'
+import { checkDesignatedSite } from './common.js'
 
 const { DESIGNATED_SITE_START, DESIGNATED_SITE_NAME } = conservationConsiderationURIs
 
 export default pageRoute({
   page: DESIGNATED_SITE_START.page,
   uri: DESIGNATED_SITE_START.uri,
-  checkData: checkApplication,
+  checkData: [checkApplication, checkDesignatedSite],
   completion: DESIGNATED_SITE_NAME.uri
 })
