746b9bc6e76b2d0c08ca6b245c411686230ea66d
Richard Mohammed <richard.mohammed@defra.gov.uk>
2023-11-10 14:32:27 +0000

    Remove empty functions

--- packages/web-service/src/pages/feedback/feedback.js
+++ packages/web-service/src/pages/feedback/feedback.js
@@ -1,24 +1,7 @@
 import pageRoute from '../../routes/page-route.js'
 import { FEEDBACK } from '../../uris.js'
 
-export const getData = async request => {
-  return {
-  }
-}
-
-export const completion = async request => {
-  // TODO - Navigate to feedback confirmation page
-  return ''
-}
-
-export const validator = async (payload, context) => {
-  // TODO - Add validation code
-}
-
 export default pageRoute({
   page: FEEDBACK.page,
-  uri: FEEDBACK.uri,
-  validator,
-  completion,
-  getData
+  uri: FEEDBACK.uri
 })
