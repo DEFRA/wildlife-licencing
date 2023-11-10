c74965c90f5c5254d7d26c6d88cb5ae503654bd9
Richard Mohammed <richard.mohammed@defra.gov.uk>
2023-11-10 15:22:02 +0000

    SDDSIP-1847 - Add validation to feedback page

--- packages/web-service/src/pages/feedback/feedback.js
+++ packages/web-service/src/pages/feedback/feedback.js
@@ -1,7 +1,18 @@
 import pageRoute from '../../routes/page-route.js'
 import { FEEDBACK } from '../../uris.js'
+import Joi from 'joi'
+
+export const validator = async payload => {
+  Joi.assert(
+    payload,
+    Joi.object({
+      'nps-satisfaction': Joi.any().required()
+    }).options({ abortEarly: false, allowUnknown: true })
+  )
+}
 
 export default pageRoute({
   page: FEEDBACK.page,
-  uri: FEEDBACK.uri
+  uri: FEEDBACK.uri,
+  validator
 })
