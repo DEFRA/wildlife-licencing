687d064c831d075291c46d1ee713ea0fa96f4300
Richard Mohammed <richard.mohammed@defra.gov.uk>
2023-11-20 16:45:18 +0000

    SDDSIP-1849 - Fix code smells

--- packages/api/src/handlers/feedback/common.js
+++ packages/api/src/handlers/feedback/common.js
@@ -1,5 +1,3 @@
 import { PowerPlatformKeys } from '@defra/wls-powerapps-keys'
 
-export const getSddsRatingValue = rating => {
-  return PowerPlatformKeys.FEEDBACK_RATINGS[rating]
-}
+export const getSddsRatingValue = rating => PowerPlatformKeys.FEEDBACK_RATINGS[rating]
