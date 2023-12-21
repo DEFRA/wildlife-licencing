21ccaa5b5b80eb8dd241f9b7ceb970c66dfcb04d
Richard Mohammed <richard.mohammed@defra.gov.uk>
2023-12-21 16:50:23 +0000

    SDDSIP-1748 - Fix daylight savings issue for formatted date time

--- packages/web-service/src/pages/common/common.js
+++ packages/web-service/src/pages/common/common.js
@@ -1,9 +1,9 @@
 export const timestampFormatter = ts => ts
-  ? Intl.DateTimeFormat('en-GB', { year: 'numeric', month: 'long', day: 'numeric' }).format(new Date(ts))
+  ? Intl.DateTimeFormat('en-GB', { timeZone: 'UTC', year: 'numeric', month: 'long', day: 'numeric' }).format(new Date(ts))
   : null
 
 export const timestampFormatterWithTime = ts => ts
-  ? Intl.DateTimeFormat('en-GB', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true }).format(new Date(ts))
+  ? Intl.DateTimeFormat('en-GB', { timeZone: 'UTC', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true }).format(new Date(ts))
   : null
 
 export const yesNoFromBool = a => {
