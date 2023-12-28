c431e2bc0fd1f232c3396f9a348343ac03cfa26e
jonathangoulding <jonathangoulding21@gmail.com>
2023-12-28 09:18:47 +0000

    fix: user role in journey

--- packages/web-service/src/pages/window-not-open/window-not-open.js
+++ packages/web-service/src/pages/window-not-open/window-not-open.js
@@ -1,9 +1,9 @@
 import pageRoute from '../../routes/page-route.js'
-import { eligibilityURIs, WINDOW_NOT_OPEN } from '../../uris.js'
+import { WINDOW_NOT_OPEN, USER_ROLE } from '../../uris.js'
 
 export const windowNotOpen = pageRoute({
   page: WINDOW_NOT_OPEN.page,
   uri: WINDOW_NOT_OPEN.uri,
-  completion: eligibilityURIs.LANDOWNER.uri,
+  completion: USER_ROLE.uri,
   options: { auth: { mode: 'optional' } }
 })
