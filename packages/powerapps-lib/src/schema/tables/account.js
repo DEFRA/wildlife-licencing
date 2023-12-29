ec892a6d8e109344a064234e5e59811375d0824f
Richard Mohammed <richard.mohammed@defra.gov.uk>
2023-12-29 14:40:00 +0000

    SDDSIP-1784 - Add DEFRA customer account and contact ids to mappings

--- packages/powerapps-lib/src/schema/tables/account.js
+++ packages/powerapps-lib/src/schema/tables/account.js
@@ -5,5 +5,6 @@ export const Account = new Table('accounts', [
   new Column('sdds_sourceremote', null, () => true, null, OperationType.INBOUND_AND_OUTBOUND, () => 'sdds_sourceremote eq true'),
   new Column('name', 'name'),
   new Column('emailaddress1', 'contactDetails.email'),
+  new Column('sdds_uuid', 'organisationId'),
   ...address
 ], null, 'accounts', 'accounts', 'accountid')
