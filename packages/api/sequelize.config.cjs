d8220ad21cca0b27cd6d1dbf2818853f963e08c0
Richard Mohammed <richard.mohammed@defra.gov.uk>
2024-02-13 18:02:33 +0000

    SDDSIP-2058 - Sequelize config file updated to include test and production

--- packages/api/sequelize.config.cjs
+++ packages/api/sequelize.config.cjs
@@ -1,9 +1,32 @@
+const POSTGRES_USER = process.env.POSTGRES_USER
+const POSTGRES_PW = process.env.POSTGRES_PW
+const POSTGRES_DB = process.env.POSTGRES_DB
+const POSTGRES_HOST = process.env.POSTGRES_HOST
+const POSTGRES_PORT = process.env.POSTGRES_PORT ? process.env.POSTGRES_PORT : 5432
+
 module.exports = {
   development: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PW,
-    database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST,
+    username: POSTGRES_USER,
+    password: POSTGRES_PW,
+    database: POSTGRES_DB,
+    host: POSTGRES_HOST,
+    port: POSTGRES_PORT,
+    dialect: 'postgres'
+  },
+  test: {
+    username: POSTGRES_USER,
+    password: POSTGRES_PW,
+    database: POSTGRES_DB,
+    host: POSTGRES_HOST,
+    port: POSTGRES_PORT,
+    dialect: 'postgres'
+  },
+  production: {
+    username: POSTGRES_USER,
+    password: POSTGRES_PW,
+    database: POSTGRES_DB,
+    host: POSTGRES_HOST,
+    port: POSTGRES_PORT,
     dialect: 'postgres'
   }
 }
