fe6d3f2a3c7d960080d8502eaa6fc51ca72c9a7a
Richard Mohammed <richard.mohammed@defra.gov.uk>
2024-04-04 12:59:59 +0100

    SDDSIP-2045: Update tests for code coverage

--- packages/web-service/src/handlers/__tests__/cookie-prefs.spec.js
+++ packages/web-service/src/handlers/__tests__/cookie-prefs.spec.js
@@ -51,4 +51,36 @@ describe('the cookie-prefs handler function', () => {
     expect(mockUpdate).toHaveBeenCalledWith('6829ad54-bab7-4a78-8ca9-dcf722117a45',
       { cookiePrefs: { analytics: true }, id: '6829ad54-bab7-4a78-8ca9-dcf722117a45' })
   })
+
+  it('updates cookiePrefs if hideMessage query parameter is truthy and user is signed in', async () => {
+    const mockGetById = jest.fn(() => ({ id: '6829ad54-bab7-4a78-8ca9-dcf722117a45', cookiePrefs: {} }))
+    const mockUpdate = jest.fn()
+    const mockRedirect = jest.fn()
+    const mockGetData = jest.fn(() => ({
+      userId: '6829ad54-bab7-4a78-8ca9-dcf722117a45'
+    }))
+    const mockSetData = jest.fn()
+
+    const request = {
+      query: { hideMessage: 'yes' },
+      info: { referrer: 'http://0.0.0.0/foo' },
+      cache: () => ({
+        getData: mockGetData,
+        setData: mockSetData
+      })
+    }
+
+    jest.doMock('../../services/api-requests.js', () => ({
+      APIRequests: {
+        USER: {
+          getById: mockGetById,
+          update: mockUpdate
+        }
+      }
+    }))
+    const cookiePrefs = (await import('../cookie-prefs.js')).default
+    await cookiePrefs(request, { redirect: mockRedirect })
+    expect(mockUpdate).toHaveBeenCalledWith('6829ad54-bab7-4a78-8ca9-dcf722117a45',
+      { cookiePrefs: { hideMessage: true }, id: '6829ad54-bab7-4a78-8ca9-dcf722117a45' })
+  })
 })
